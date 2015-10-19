a6fc30d01fd838bd183523bbea76963fd482e898
biud436 <biud436@gmail.com>
2015-10-20 03:15:55 +0900

    원형 타이틀 메뉴 플러그인

--- RotateMenu.js
+++ RotateMenu.js
@@ -1,7 +1,7 @@
 /**
  * @author biud436
  * @date 2015.10.17
- * @version 1.1
+ * @version 1.2
  */
  
 var oRSUtils = function() { 
@@ -66,113 +66,147 @@ Array.prototype.min = function() {
  
     var _alias_start = Scene_Title.prototype.start,
           _alias_createCommandWindow = Scene_Title.prototype.createCommandWindow,
-          _alias_update = Scene_Title.prototype.update;
+          _alias_update = Scene_Title.prototype.update,
+		  _alias_startFadeOut = Scene_Title.prototype.startFadeOut;
  
     Scene_Title.prototype.start = function() {
         _alias_start.call(this);
-        
+        this.initUtils();
+		this.initSpriteParameter();
+		this.initTouchParameter();
+        this.makeSprite();
+    };
+	
+	Scene_Title.prototype.initUtils = function() {
         if(this._utils == null) {
             this._utils = new oRSUtils();
         }
-        
+	};
+	
+	Scene_Title.prototype.initSpriteParameter = function() {
         this._max = 1;
-        this._rotateLeft = this._rotateRight = this._isGameStarted = false;
+        this._rotateLeft = false;
+		this._rotateRight = false;
+		this._isGameStarted = false;
         this._originPosition = [this._utils.X, this._utils.Y];
         this._r = 3;
         this._angle = 0.0;
-		
-		this._firstPos = {x: 0, y: 0};
-		this._lastPos = {x: 0, y: 0};
+	};
+	
+	Scene_Title.prototype.initTouchParameter = function() {
+		this._firstPos = 0;
+		this._lastPos = 0;
 		this._x_velocity = 0;
 		this._y_velocity = 0;
-		this._touch_velocity = false
-    
-        this.makeSprite();
-    };
-    
-    Scene_Title.prototype.update = function() {
-        _alias_update.call(this);        
-        
-        if(this._textCreated) { 
-            this.updateSprite() ; 
-        }
-        
-		if(TouchInput.isMoved() && !this._touch_velocity) {
-		
-			this._firstPos = 
-			{
-				x: TouchInput.x, 
-				y: TouchInput.y
-			};
-			
+		this._touch_velocity = false;	
+	};
+	
+	Scene_Title.prototype.updateTouchInput = function() {
+		if(TouchInput.isTriggered() && !this._touch_velocity) {
+			this._firstPos = TouchInput.x;
 			this._touch_velocity = true;
 		}	
 		else	if(TouchInput.isReleased() && this._touch_velocity) {
-			this._lastPos = 
-			{
-				x: TouchInput.x, 
-				y: TouchInput.y
-			};
-			
-			this._x_velocity = this._lastPos.x - this._firstPos.y;
-			this._y_velocity = this._lastPos.x - this._firstPos.y;	
-			
-			this._x_velocity < 0? this.left(true) : this.right(true);
-			this._y_velocity < 0? this.left(true) : this.right(true);
-			
+			this._lastPos = TouchInput.x;
+			this._velocity = this._lastPos - this._firstPos;		
+			this._velocity < 0? this.left(true) : this.right(true);
 			this._touch_velocity = false;
-			
 		}
-		
-        this.left(Input.isTriggered("left"));
+	};
+    
+	Scene_Title.prototype.updateKeyboardCheck = function() {
+		this.left(Input.isTriggered("left"));
         this.right(Input.isTriggered("right") );
-        
+
         if( Input.isTriggered("ok") || TouchInput.isCancelled() ) { 
-            this.selectMenu(); 
+            this.selectMenu();
         }
+	};
+	
+    Scene_Title.prototype.update = function() {
+		this.updateSprite();
+		this.updateTouchInput();
+		this.updateKeyboardCheck();
+		_alias_update.call(this);        
     };
     
+	Scene_Title.prototype.directionCheck = function(dir) {
+	
+		var isLeft = !this._rotateRight && this._rotateLeft;
+		var radian = this._utils.convertToRadian(this._max);
+		var isRight = this._rotateRight && !this._rotateLeft;
+	
+		switch(dir) {
+		case 'left':
+			return isLeft && this._angle > radian;
+			break;
+		default:
+			return isRight && this._angle < radian;
+			break;
+		}
+	}
+	
     Scene_Title.prototype.updateSprite = function() {
-    
-        if(!this._rotateRight && this._rotateLeft && this._angle > this._utils.convertToRadian(this._max) ) {
-            this._utils.wrapAngle(this._angle -= this.upAngle());
+		if(!this._textCreated) { 
+			return;
+		}
+        if( this.directionCheck('left') ) {
+            this._utils.wrapAngle( this._angle -= this.upAngle() );
         }
-        
-        if(this._rotateRight && !this._rotateLeft && this._angle < this._utils.convertToRadian(this._max) ) {
-            this._utils.wrapAngle(this._angle += this.upAngle());
+        if( this.directionCheck('right') ) {
+            this._utils.wrapAngle( this._angle += this.upAngle() );
         }
-        
-        if(this._textCreated) {
-            this.moveMenu();
-            this.updateTone();
-            this.updateScale();
-        }
-        
+		
+		this.moveMenu();
+		this.updateScale();
+		
     };
     
-    var _alias_startFadeOut = Scene_Title.prototype.startFadeOut;
     Scene_Title.prototype.startFadeOut = function (duration, white) {
         _alias_startFadeOut.apply(this, arguments);
-        this.text1.opacity = this.text2.opacity = this.text3.opacity = 0;
+        this.text1.opacity = 0;
+		this.text2.opacity = 0;
+		this.text3.opacity = 0;
     };    
     
-    Scene_Title.prototype.updateTone = function() { 
-    
-    };
-    
     Scene_Title.prototype.updateScale = function() { 
-    
+			
+		var l = this.getTopItem();
+		
+		l.forEach( function(i) { 
+		
+		if(l.indexOf(i) === 0) {
+				i.scale.set(1.3,1.3);
+			} else {
+				i.scale.set(1.0,1.0);
+			}
+		}.bind(this) );
     };
+    
+	Scene_Title.prototype.getTopItem = function() {
+		
+		var list = [this.text1, this.text2, this.text3];
+		
+		list.sort( function(a, b) 
+		{ 
+			return Math.floor(a.y) - Math.floor(b.y) 
+		});
+		
+		return list;
+	};
     
     Scene_Title.prototype.selectMenu = function() {
-        if(this._isGameStarted == false) {
-            switch(this.menuIndex()) {
+        if(this._isGameStarted == false) 
+		{
+            switch(this.menuIndex()) 
+			{
                 case 1: 
                     this.commandNewGame();
                     this._isGameStarted = true;
                     break;
                 case 2: 
-                    if( DataManager.isAnySavefileExists() ) {
+                    if( DataManager.isAnySavefileExists() ) 
+					{
                         this.commandContinue();
                     }
                     break;
@@ -185,7 +219,8 @@ Array.prototype.min = function() {
     };
     
     Scene_Title.prototype.left= function(wrap) {
-        if(wrap) {
+        if(wrap)
+		{
             SoundManager.playCursor();
             this._rotateLeft = true;
             this._rotateRight = false;
@@ -194,7 +229,8 @@ Array.prototype.min = function() {
     };
     
     Scene_Title.prototype.right = function(wrap) {
-        if(wrap) {
+        if(wrap) 
+		{
             SoundManager.playCursor();
             this._rotateLeft = false;
             this._rotateRight = true;
@@ -218,19 +254,20 @@ Array.prototype.min = function() {
     };
     
     Scene_Title.prototype.spriteDistance = function() {
-        var    a = this.text1.position.y - this._originPosition[1],
-                b = this.text2.position.y - this._originPosition[1],
-                c = this.text3.position.y - this._originPosition[1],
-              result = [a,b,c];
+        var a, b, c; 
+		a = this.text1.position.y - this._originPosition[1];
+		b = this.text2.position.y - this._originPosition[1];
+		c = this.text3.position.y - this._originPosition[1];
+        result = [a,b,c];
         return result;
     };
     
     Scene_Title.prototype.move = function(sprite, r, angle) {
-        var    x = ( this._originPosition[0] )+ r * Math.cos(angle) - sprite.width / 2,
-                y = ( this._originPosition[1] ) + r * Math.sin(angle) - sprite.height / 2;
+        var x = ( this._originPosition[0] )+ r * Math.cos(angle) - sprite.width / 2,
+        y = ( this._originPosition[1] ) + r * Math.sin(angle) - sprite.height / 2;
         sprite.position.x = x;
         sprite.position.y = y;
-    };        
+    };
     
     Scene_Title.prototype.makeSprite = function() {
         this.text1 = this.makeText("Game Start");
@@ -240,13 +277,14 @@ Array.prototype.min = function() {
     };
     
     Scene_Title.prototype.makeText = function(str) {
-        var text = new Sprite();
+        var text = new Sprite_Button();
         text.bitmap = new Bitmap(100, 48);
         this.addChild(text);
         var rect = text.bitmap.rect;
         text.bitmap.drawText(String(str), rect.x, rect.y, rect.width, rect.height);
+		text.setClickHandler(this.selectMenu.bind(this));
         return text;
-    };    
+    };
     
      Scene_Title.prototype.createCommandWindow = function() {
        _alias_createCommandWindow.call(this);
@@ -257,5 +295,3 @@ Array.prototype.min = function() {
     };    
     
 })();
- 
-SceneManager.goto(Scene_Title);
