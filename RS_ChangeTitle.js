f5d9399f809db0f4f29c416bbb544770947f08db
biud436 <biud436@gmail.com>
2016-02-20 00:43:59 +0900

    .

---
+++ RS_ChangeTitle.js
@@ -0,0 +1,47 @@
+/*:
+ * RS_ChangeTitle.js
+ * @plugindesc This plugin changes the title screen image after a certain time.
+ * @author biud436
+ * @date 2015.11.09
+ * 
+ * @param FILE
+ * @desc Write the list of the title screen image. (Distinguish to the blank)
+ * @default Book Castle CrossedSwords Crystal DemonCastle
+ * 
+ * @param TIME
+ * @desc frame
+ * @default 2 
+ *
+ */
+ 
+(function() {
+
+  var parameters = PluginManager.parameters('RS_ChangeTitle');
+  var titleFile = (function() { 
+    return parameters['FILE'].split(/[^\w]/gi);
+  })();
+  var titleTime = Number(parameters['Time'] || 2);
+  
+  var _Scene_Title_update = Scene_Title.prototype.update;
+  Scene_Title.prototype.update = function() {
+    _Scene_Title_update.call(this);
+    this.chooseIndex();
+  };
+  
+  if(!!Graphics) {
+    Graphics.frameRate = function() {
+      return Graphics._fpsMeter.fps || 60;
+    }  
+  }
+  
+  Scene_Title.prototype.getTime = function() {
+    return ((Graphics.frameCount / Graphics.frameRate() ) | 0);
+  };
+  
+  Scene_Title.prototype.chooseIndex = function() {
+    if(this.getTime() % titleTime > 0) return;
+    this._spriteIndex = (this.getTime() % titleFile.length) || 0;
+    this._backSprite1.bitmap = ImageManager.loadTitle1(titleFile[this._spriteIndex]);    
+  };  
+
+})();
---
+++ RS_SavefileList.js
@@ -0,0 +1,34 @@
+/*:
+ * RS_SaveFileList.js
+ * @plugindesc This plugin displays a face graphic on the save & load screen.
+ * @author biud436
+ */
+
+(function() {
+
+Window_SavefileList.prototype.drawPartyCharacters = function(info, x, y) {
+    if (info.characters) {
+        for (var i = 0; i < info.faces.length; i++) {
+            var data = info.faces[i];
+            this.drawFace(data[0], data[1], x + i * 78, this.standardPadding(), 144, 144);
+        }
+    }
+};
+
+Window_SavefileList.prototype.drawFace = function(faceName, faceIndex, x, y, width, height) {
+    width = width || Window_Base._faceWidth;
+    height = height || Window_Base._faceHeight;
+    var bitmap = ImageManager.loadFace(faceName);
+    var pw = Window_Base._faceWidth;
+    var ph = Window_Base._faceHeight;
+    var sw = Math.min(width, pw);
+    var sh = Math.min(height, ph);
+    var dx = Math.floor(x + Math.max(width - pw, 0) / 2);
+    var dy = Math.floor(y + Math.max(height - ph, 0) / 2);
+    var sx = faceIndex % 4 * pw + (pw - sw) / 2;
+    var sy = Math.floor(faceIndex / 4) * ph + (ph - sh) / 2;
+    var itemWH = this.itemHeight() - this.standardPadding();
+    this.contents.blt(bitmap, sx, sy, sw, sh, dx, dy, itemWH, itemWH );
+};
+
+})();
