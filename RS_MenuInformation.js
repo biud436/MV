7baaa611ee3fb594086dc6060c671973ca467aad
biud436 <biud436@gmail.com>
2016-02-21 22:13:29 +0900

    .

---
+++ RS_MenuInformation.js
@@ -0,0 +1,165 @@
+/*:
+ * RS_MenuInformation.js
+ * @plugindesc RS_MenuInformation v1.1
+ * @author biud436
+ *
+ * @desc Menu Name
+ * @param Menu Name
+ * @default Information
+ *
+ */
+
+var RS = RS || {};
+
+function Window_Information() {
+   this.initialize.apply(this, arguments);
+}
+
+function Scene_Information() {
+   this.initialize.apply(this, arguments);
+}
+
+//============================================================================
+// RS.MenuInformation
+//============================================================================
+RS.MenuInformation = RS.MenuInformation || function() {
+  this._texts = [];
+  this.add = function(text) {
+    this._texts.push(text)
+  }
+  this.clear = function() {
+    if(this._texts.length > 0) {
+      this._texts = [];
+    }
+  }
+  this.allText = function() {
+    if(this._texts.length > 0) {
+      return this._texts.reduce(function(cur, now) {
+       return cur + '\n' + now;
+      });
+    } else {
+      return '';
+    }
+  }
+};
+
+(function() {
+
+  var parameters = PluginManager.parameters('RS_MenuInformation');
+  RS.MenuInformation._menuName = String(parameters['Menu Name'] || 'Information');
+  RS.MenuInformation._menuSymbol = 'information';
+
+  //============================================================================
+  // Window_MenuCommand
+  //============================================================================
+  Window_MenuCommand.prototype.makeCommandList = function() {
+      this.addMainCommands();
+      this.addFormationCommand();
+      this.addOriginalCommands();
+      this.addOptionsCommand();
+      this.addInformationCommand();
+      this.addSaveCommand();
+      this.addGameEndCommand();
+  };
+
+  Window_MenuCommand.prototype.addInformationCommand = function() {
+    this.addCommand(RS.MenuInformation._menuName, RS.MenuInformation._menuSymbol , true);
+  }
+
+  //============================================================================
+  // Scene_Menu
+  //============================================================================
+  var alias_Scene_Menu_createCommandWindow = Scene_Menu.prototype.createCommandWindow;
+  Scene_Menu.prototype.createCommandWindow = function() {
+    alias_Scene_Menu_createCommandWindow.call(this);
+    this._commandWindow.setHandler(RS.MenuInformation._menuSymbol, this.commandInformation.bind(this));
+  };
+
+  Scene_Menu.prototype.commandInformation = function() {
+      SceneManager.push(Scene_Information);
+  };
+
+  //============================================================================
+  // Window_Information
+  //============================================================================
+  Window_Information.prototype = Object.create(Window_Base.prototype);
+  Window_Information.prototype.constructor = Window_Information;
+
+  Window_Information.prototype.initialize = function(numLines) {
+      var width = Graphics.boxWidth;
+      var height = Graphics.boxHeight;
+      Window_Base.prototype.initialize.call(this, 0, 0, width, height);
+      this._text = '';
+  };
+
+  Window_Information.prototype.setText = function(text) {
+      if (this._text !== text) {
+          this._text = text;
+          this.refresh();
+      }
+  };
+
+  Window_Information.prototype.clear = function() {
+      this.setText('');
+  };
+
+  Window_Information.prototype.refresh = function() {
+      this.contents.clear();
+      this.drawTextEx(this._text, this.textPadding(), 0);
+  };
+
+  //============================================================================
+  // Scene_Information
+  //============================================================================
+  Scene_Information.prototype = Object.create(Scene_MenuBase.prototype);
+  Scene_Information.prototype.constructor = Scene_Information;
+
+  Scene_Information.prototype.initialize = function() {
+      Scene_MenuBase.prototype.initialize.call(this);
+  };
+
+  Scene_Information.prototype.create = function() {
+      Scene_MenuBase.prototype.create.call(this);
+      this._informationWindow = new Window_Information();
+      this.addWindow(this._informationWindow);
+      this.refresh();
+  };
+
+  Scene_Information.prototype.update = function() {
+    Scene_MenuBase.prototype.update.call(this);
+    if(this.isCancelled()) {
+      SceneManager.pop();
+    }
+  }
+
+  Scene_Information.prototype.isCancelled = function() {
+      return Input.isTriggered('menu') || TouchInput.isCancelled();
+  };
+
+  Scene_Information.prototype.refresh = function() {
+      var actor = this.actor();
+      this._informationWindow.setText(RS.MenuInformation.allText());
+      this._informationWindow.refresh();
+      this._informationWindow.activate();
+  };
+
+  //============================================================================
+  // Game_Interpreter
+  //============================================================================
+
+  var alias_Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+  Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    alias_Game_Interpreter_pluginCommand.call(this, command, args);
+    if(command === "MenuInformation") {
+      switch (args[0].toLowerCase()) {
+      case 'add':
+        RS.MenuInformation.add(args[1]);
+        break;
+      case 'clear':
+        RS.MenuInformation.clear();
+        break;
+      }
+    }
+  };
+
+})();
