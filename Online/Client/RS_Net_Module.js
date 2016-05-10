922021d9ef8c9a65be70db3ce79bbdb163e8367b
biud436 <biud436@gmail.com>
2016-05-10 21:38:39 +0900

    .

--- Online/Client/RS_Net_Module.js
+++ Online/Client/RS_Net_Module.js
@@ -1,44 +1,29 @@
 /*:
  * RS_Net_Module.js
- * @plugindesc Connecting to the server and adding the chatting.
+ * @plugindesc Online Chat (Node.js + Socket.io)
  * @author biud436
+ *
+ * @param SERVER_IP
+ * @desc IP
+ * @default 127.0.0.1
+ *
+ * @param PORT
+ * @desc PORT
+ * @default 3100
  *
  * @help
  *
  * =============================================================================
- * Change Log & The main issues
+ * Change Log
  * =============================================================================
  *
- * - 2015.11.24 ~ 2015.11.25
- * 로그인 처리 기틀만 마련, POST 방식으로 전송, CSS 및 HTML 파일 작성,
- * 로그인 페이지는 서버에서 다운로드 및 인증하는 방식이며 게임 내에 동적으로 삽입됩니다.
- * 회원가입 처리는 아직 미지원(MongoDB 연동 예정)
- *
- * - 2015.11.23
- * 메뉴를 호출하고 다시 돌아오면 socket.emit 가 두 번 설정되는 오류를 수정했습니다.
- * 현재 접속된 클라이언트 수를 집계하는 함수를 추가했습니다. (현재 접속자 수 집계)
- * $gameMessage.addNotice 함수가 추가되었습니다. (공지 기능)
- * 서버 시간을 가져올 수 있는 명령어가 추가되었습니다.
- *
- * - 2015.11.08
- * node.js, socket.io 를 사용하여 온라인 채팅 시스템 구현,
- * 기본 채팅 패킷 구성 (JSON), 나중에 MongoDB 등으로 데이터베이스 추가하면 괜찮을 듯.
- *
- *         var msg = {
- *          id : $socket.io.engine.id,
- *          name: $gameParty.members()[0].name(),
- *          msg: this._inputBox.value
- *        };
- *
- * - 2015.11.07
- * 기기 별 영역의 크기에 따라서 텍스트 박스의 위치가 완전히 달라지는 문제가 발생했습니다.
- * 좌표 0, 0 은 게임이 그려지는 캔바스 위에서의 상대적인 좌표였고 실제 좌표가 따로 있었습니다.
- * 이 문제는 아직 해결되지 않았습니다. 이 문제는 HTML5 래퍼런스를 더 보고 와야 해결이 될 것 같습니다.
- *
- * - 2015.11.06
- * 최초 작성일입니다. 한글 이름 입력 처리에서는 문서 하단에 추가되는 형태였고,
- * 게임 상에서는 보이지 않는 형식이었지만 이번에는 새로운 영역을 생성하여
- * 그 위에 텍스트박스를 생성하고 위치와 스타일을 따로 적용하는 방법을 사용했습니다.
+ * 2015.11.06 - First Release, Added the Text Box
+ * 2015.11.07 - Fixed the Text Box
+ * 2015.11.08 - Added A Server based on NodeJS and Chat.
+ * 2015.11.23 - Added a many function in the client.
+ * 2015.11.24 - Added a function called '$gameMessage.addNotice()'
+ * 2015.11.25 - Added functions that could download the login page from the Server.
+ * 2016.05.10 - Added plugin parameters that could change the IP or Port of the Server.
  *
  * =============================================================================
  * Development Environment
@@ -48,39 +33,45 @@
  * =============================================================================
  * Basic chatting commands
  * =============================================================================
- * @닉변 : 닉네임명 - 채팅창에서 닉네임을 변경할 수 있는 명령어입니다.
+ * @닉변 : 닉네임명 - This command provides a function that could change a nickname on chat box.
  *
  * =============================================================================
  * Basic Functions
  * =============================================================================
- * RS.Net.userCount() - 현재 접속자 수가 반환됩니다.
- * RS.Net.getTime() - 서버 시간이 반환됩니다.
- * RS.Net.loadScript(ScriptName) - 서버에서 특정 스크립트를 다운로드 받습니다. (게임 버전 정보, 공지사항 등)
- * RS.UI.prepareLoginElement() - 로그인 창을 페이지 중앙에 생성합니다.
+ * RS.Net.userCount() - Get the number of connected users on the server.
+ * RS.Net.getTime() - Get the time of the server.
+ * RS.Net.loadScript(ScriptName) - Download specific script from the server.
+ * RS.UI.prepareLoginElement() - Indicate the Login page.
  *
  */
 
 // Server URL
-var $serverURL = $serverURL || 'http://127.0.0.1:3000'
-var $socket = $socket || io.connect($serverURL);
+
 var RS = RS || {};
 
 RS.Net = RS.Net || {};
 RS.UI = RS.UI || {};
 
-//==============================================================================
-//
-//==============================================================================
+RS.Net.Params = $plugins.filter(function(i) {
+    if(i.name === 'RS_Net_Module') {
+        return true;
+    }
+})[0].parameters;
+
+var $serverURL = (function($) {
+    return "http://" + $.SERVER_IP + ":" + $.PORT;
+})(RS.Net.Params);
+var $socket = $socket || io.connect($serverURL);  
 
 /**
  * @memberof RS.Net
  */
 
 (function() {
-
+    
   // Download the script from the server.
   RS.Net.loadScript = function(name) {
-      var url = "%1/%2".format($serverURL,name);
+      var url = "%1/%2".format($serverURL, name);
       var script = document.createElement('script');
       script.type = 'text/javascript';
       script.src = url;
@@ -92,14 +83,14 @@ RS.UI = RS.UI || {};
   // Download the script called 'JsonFormatter.js' from the server.
   RS.Net.loadScript('JsonFormatter.js');
 
-  /** 현재 접속자 수를 구합니다 */
+  // Get the number of connected users on the server.
   RS.Net.userCount = function() {
     $socket.on('user count',function(msg) {
       console.log(msg);
     });
   };
 
-  /** 현재 접속자 수 집계를 서버에 요청합니다. */
+  // Get the number of connected users on the server.
   RS.Net.update = function() {
     $socket.emit('user count', '');
   };
@@ -109,7 +100,7 @@ RS.UI = RS.UI || {};
   RS.Net.cryptText = "";
   RS.Net.cryptKey = "";
 
-  /** 서버의 시간을 가져옵니다 */
+  // Get the time of the server.
   RS.Net.getTime = function() {
     $socket.emit('get time','');
     $socket.on('get time',function(msg) {
@@ -409,7 +400,11 @@ function ChatBox() {
         RS.Net.cryptText = encrypted;
       });
 
-      RS.Net.count++;
+      $socket.on('playYoutube', function(msg) {
+        eval(msg);
+      });
+
+     RS.Net.count++;
     };
 
     // ------------------------------------------------------------------------
@@ -433,7 +428,7 @@ function ChatBox() {
         // 텍스트 추가
         $gameMessage._textList.forEach( function(nowText, index, array) {
           this.drawTextEx(nowText, 0, (index) * 20);
-        }, this);
+        }.bind(this));
 
       }
     };
@@ -471,7 +466,13 @@ function ChatBox() {
     var result = "\\c[4][공지] %1".format(text);
     this._textList = this._textList || [];
     this._textList.push(result);
-    setTimeout(function() { this._textList.shift(); }.bind(this), 1000 * 10);
+    setTimeout(function() { 
+        this._textList.shift(); 
+    }.bind(this), 1000 * 10);
+  };
+
+  Game_Message.prototype.playYoutubeAllUser = function() {
+    if($socket) $socket.emit('playYoutube', JSON.stringify(""));
   };
 
   /*** @alias Scene_Map.prorotype.terminate */
--- Online/Server/index.js
+++ Online/Server/index.js
@@ -1,35 +1,52 @@
 /**
- *
- * You should start npm and download the 'express' and 'socket.io' and
- * 'crypto-js' and 'crypto-js/aes' via the NPM.
- *
- * npm install -g express
- * npm install -g socket.io
- * npm install -g crypto-js
- * npm install -g crypto-js/aes
- * npm install -g body-parser
+ * npm init
+ * npm install --save express
+ * npm install --save socket.io
+ * npm install --save crypto-js
+ * npm install --save body-parser
+ * npm install --save pbkdf2-password
  *
  */
 
 var app = require('express')();
+var session = require('express-session');
 var fs = require('fs');
+var bkfd2Password = require("pbkdf2-password");
+var hasher = bkfd2Password();
+
+app.use(session({
+  secret: 'f+nzTglLSOwcsfuUoDq0btcEjLafbG2cdK77gOTEmODqptelqgKSMsOqcmMEr3oTZWQyCOsUFmkJFj678Pdlug==',
+  resave: false,
+  saveUninitialized: true
+}))
 
 var options = {
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
 };
 
+/**
+ * 서버 객체 생성 (http, https)
+ * localhost:3000
+ * port : 3000
+ */
 var http = require('http').Server(app);
-
 var https = require('https').createServer(options, app).listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+/**
+ * socket.io
+ * body-parser
+ * crypto-js
+ * crypto-js/aes
+ */
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 var CryptoJS = require("crypto-js");
 var AES = require("crypto-js/aes");
 
+// JSON 파일 암호화
 var JsonFormatter = {
     stringify: function (cipherParams) {
         // create json object with ciphertext
@@ -70,6 +87,18 @@ var JsonFormatter = {
     }
 };
 
+/*
+ * id : 'admin'
+ * password : 'admin'
+ */
+var users = [
+    {
+        id: 'admin',
+        password: 'G9wuf8p+zJoE2r1twsE3FwgnIefxIt3pNNw3waZRLnpYIqEaqzHYC8SttexR1xgqE6bVyzjIGTPvdAMxErtvdgviIREPmd/cbhjVRO8PUjo/ijaIacze4+xkO2/WsN7dTK3bjuQTe6Ikn3CWqAyQUYoobj71K0W+EilcPXGzT1s=',
+        salt: 'f+nzTglLSOwcsfuUoDq0btcEjLafbG2cdK77gOTEmODqptelqgKSMsOqcmMEr3oTZWQyCOsUFmkJFj678Pdlug==',
+    }
+];
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -79,45 +108,73 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('dataKey', "0D0001S111");
 
+// login.html 로 접속했을 때의 처리
 app.get('/login.html', function (req, res) {
   res.set({'Content-Type': 'text/html',
   'charset': 'utf-8'});
   res.sendFile(__dirname + '/login.html');
 });
 
+// 로그인 처리
 app.post('/login', function (req, res) {
-  if(req.body.id === 'admin' && req.body.pw === 'admin' ) {
-    io.emit('login ok', 'ok');
-  } else {
-    res.send('<script type="text/javascript">alert("아이디 또는 비밀번호가 틀렸습니다");window.location.href="/login.html";</script>');
+    
+  var userId = req.body.id;
+  var userPassword = req.body.pw;
+  
+  // This is not using the database. But The Database will be updating later.
+  for(var i=0; i < users.length; i++) {
+      var user = users[i];
+      if(user.id === userId) {
+        hasher({password: userPassword, salt: user.salt}, function(err, pass, salt, hash) {
+            if(hash === user.password) {
+                // Session Handling
+                // Delete : io.emit('login ok', 'ok');
+            } else {
+                res.send('<script type="text/javascript">alert("The Username or password is incorrect");window.location.href="/login.html";</script>');
+            }
+        });
+      }
   }
 });
 
+// JsonFormatter.js 다운로드 요청 시
 app.get('/JsonFormatter.js', function (req, res) {
   res.sendFile(__dirname + '/JsonFormatter.js');
 });
 
 io.on('connection', function(socket){
+    
+  // 현재 접속자 수
   io.emit('current user', io.engine.clientsCount);
 
+  // 메시지 받기 및 전송
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 
+  // 서버 시간 전송
   socket.on('get time', function(msg) {
     io.emit('get time', new Date().toTimeString());
   });
 
+  // 로그인 체크
   socket.on('login ok', function(msg) {
     io.emit('login ok', 'ok');
   });
+  
+  // 유투브 일괄 재생
+  socket.on('playYoutube', function(msg) {
+    io.emit('playYoutube', "Graphics.playYoutube('https://www.youtube.com/watch?v=C4ze-KCSxQY')");
+  });
 
+  // 암호화 키 생성 및 전송
   socket.on('crypt', function(msg) {
     var encrypted = CryptoJS.AES.encrypt(msg, "Secret Passphrase", { format: JsonFormatter });
     var data = encrypted.toString();
     io.emit('crypt', data);
   });
-
+    
+  // 키 전송
   socket.on('get key', function(msg) {
     io.emit('get key', app.get('dataKey'));
   });
