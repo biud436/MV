1452e921290ab2dfdaf258864a62e8ce9ca0ae1f
biud436 <biud436@gmail.com>
2016-06-24 17:33:56 +0900

    test

--- Laboratory/cordova-file-api-test/scripts.js
+++ Laboratory/cordova-file-api-test/scripts.js
@@ -1,7 +1,7 @@
 
 var logOb;
 
-window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
+window.resolveLocalFileSystemURL(cordova.file.externalCacheDirectory, function(dir) {
     console.log("got main dir",dir);
     dir.getFile("log.txt", {create:true}, function(file) {
         console.log("got the file", file);
@@ -23,3 +23,47 @@ function writeLog(str) {
         console.log("ok, in theory i worked");
     }, fail);
 }
+
+function writeFile(fileEntry, dataObj) {
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function (fileWriter) {
+
+        fileWriter.onwriteend = function() {
+            console.log("Successful file read...");
+            readFile(fileEntry);
+        };
+
+        fileWriter.onerror = function (e) {
+            console.log("Failed file read: " + e.toString());
+        };
+
+        // If data object is not passed in,
+        // create a new Blob instead.
+        if (!dataObj) {
+            dataObj = new Blob(['some file data'], { type: 'text/plain' });
+        }
+
+        fileWriter.write(dataObj);
+    });
+}
+
+function readFile(fileEntry) {
+
+    fileEntry.file(function (file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+            console.log("Successful file read: " + this.result);
+            displayFileData(fileEntry.fullPath + ": " + this.result);
+        };
+
+        reader.readAsText(file);
+
+    }, onErrorReadFile);
+}
+
+window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (dirEntry) {
+    console.log('file system open: ' + dirEntry.name);
+    var isAppend = true;
+    createFile(dirEntry, "fileToAppend.txt", isAppend);
+}, onErrorLoadFs);
