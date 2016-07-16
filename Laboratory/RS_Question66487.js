72ffa5b00226b7aeb2e0516075181faed24c36bd
biud436 <biud436@gmail.com>
2016-07-17 07:08:24 +0900

    .

--- Laboratory/RS_Question66487.js
+++ Laboratory/RS_Question66487.js
@@ -1,12 +1,12 @@
 /*:
  * RS_Question66487.js
- * @plugindesc 기본 공격 애니메이션을 Class ID에 따라 변경합니다.
+ * @plugindesc 기본 공격 스킬을 Class ID에 따라 변경합니다.
  * @author biud436
  *
  * @help
  * Class 의 Note 란에 아래를 기입하세요.
  * <NormalAttackID:x>
- * x 는 애니메이션의 ID 입니다.
+ * x 는 스킬의 ID 입니다.
  */
 
 (function () {
