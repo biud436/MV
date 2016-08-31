c788cbca22c933e60266f098855e612b293bf45d
biud436 <biud436@gmail.com>
2016-09-01 02:26:54 +0900

    Added Plugin Commands

--- RS_FollowerPassable.js
+++ RS_FollowerPassable.js
@@ -1,3 +1,10 @@
+//==============================================================================
+// RS_FollowerPassable.js
+//==============================================================================
+
+var Imported = Imported || {};
+Imported.RS_FollowerPassable = true;
+
 /*:
  * RS_FollowerPassable.js
  * @plugindesc If you are using this plugin, the player will be impossible
@@ -9,19 +16,48 @@
  * @desc
  * @default true
  *
+ * @param Separate Mode
+ * @desc
+ * @default false
+ *
  * @help
- * [Plugin Command]
- * FollowerPassable enabled true
- * FollowerPassable enabled false
+ * =============================================================================
+ * Plugin Command
+ * =============================================================================
+ *
+ * FollowerPassable passable true
+ * FollowerPassable SeparateMode true
+ *
+ * The following commands could use in separate mode only.
+ *
+ * FollowerPassable setOpacity index x
+ * FollowerPassable setBlendMode index x
+ * FollowerPassable setMoveSpeed index x
+ * FollowerPassable setDirectionFix index x
+ * FollowerPassable setTransparent index x
+ *
+ * =============================================================================
+ * Change Log
+ * =============================================================================
+ * 2016.06.01 (v1.0.0) - First Release.
+ * 2016.09.01 (v1.0.4) - Added Plugin Commands
  */
 
-var Imported = Imported || {};
-Imported.RS_FollowerPassable = true;
 
 (function() {
 
   var parameters = PluginManager.parameters('RS_FollowerPassable');
-  var enabled = Boolean(parameters['Enabled'] === 'true');
+  var passable = Boolean(parameters['Enabled'] === 'true');
+  var isSeparate = Boolean(parameters['Separate Mode'] === 'true');
+
+  var KIT = Object.assign(Object.create(
+    {
+      fpSendMessage: function (index, func, args) {
+        var callFunc = $gamePlayer.followers().follower(index)[func];
+        callFunc.apply(null, args);
+      }
+    }
+  ));
 
   //============================================================================
   // Game_Player
@@ -30,7 +66,7 @@ Imported.RS_FollowerPassable = true;
   Game_Player.prototype.canPass = function(x, y, d) {
       var x2 = $gameMap.roundXWithDirection(x, d);
       var y2 = $gameMap.roundYWithDirection(y, d);
-      if(this.isFollowerPassable(x2, y2) && enabled) {
+      if(this.isFollowerPassable(x2, y2) && passable) {
         return false;
       }
       return alias_Game_Player_canPass.call(this, x, y, d);
@@ -49,6 +85,19 @@ Imported.RS_FollowerPassable = true;
       this.setThrough(false);
   };
 
+  Game_Follower.prototype.update = function() {
+      Game_Character.prototype.update.call(this);
+      if(!isSeparate) {
+        this.setMoveSpeed($gamePlayer.realMoveSpeed());
+        this.setOpacity($gamePlayer.opacity());
+        this.setBlendMode($gamePlayer.blendMode());
+        this.setWalkAnime($gamePlayer.hasWalkAnime());
+        this.setStepAnime($gamePlayer.hasStepAnime());
+        this.setDirectionFix($gamePlayer.isDirectionFixed());
+        this.setTransparent($gamePlayer.isTransparent());
+      }
+  };
+
   //============================================================================
   // Game_Followers
   Game_Followers.prototype.isFollowerPassable = function(x, y) {
@@ -64,9 +113,30 @@ Imported.RS_FollowerPassable = true;
   Game_Interpreter.prototype.pluginCommand = function(command, args) {
     alias_Game_Interpreter_pluginCommand.call(this, command, args);
     if(command === "FollowerPassable") {
-      switch (args[0].toLowerCase()) {
+      switch (args[0]) {
         case 'enabled':
-          enabled = Boolean(args[1] === 'true');
+        case 'passable':
+          passable = Boolean(args[1] === 'true');
+          break;
+        case 'SeparateMode':
+          isSeparate = Boolean(args[1] === 'true');
+          break;
+        case 'setOpacity':
+          var n = Number(args[2] || 255);
+          KIT.fpSendMessage(Number(args[1] || 0), 'setOpacity', n.clamp(0, 255) );
+          break;
+        case 'setBlendMode':
+          var type = Number(args[2] || 0);
+          KIT.fpSendMessage(Number(args[1] || 0), 'setBlendMode', type.clamp(0, 3) );
+          break;
+        case 'setMoveSpeed':
+          KIT.fpSendMessage(Number(args[1] || 0), 'setMoveSpeed', type.clamp(1, 6) );
+          break;
+        case 'setDirectionFix':
+          KIT.fpSendMessage(Number(args[1] || 0), 'setDirectionFix', args[2] === 'true' );
+          break;
+        case 'setTransparent':
+          KIT.fpSendMessage(Number(args[1] || 0), 'setTransparent', args[2] === 'true' );
           break;
       }
     }
