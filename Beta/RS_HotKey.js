5112fa0fa267dc62190e0e00fa5b27da9b5647e1
biud436 <biud436@gmail.com>
2016-08-31 23:49:58 +0900

    rewrite

--- Beta/RS_HotKey.js
+++ Beta/RS_HotKey.js
@@ -26,16 +26,81 @@ RS.HotKey = RS.HotKey || {};
     this.initialize.apply(this, arguments);
   }
 
+  //----------------------------------------------------------------------------
+  // HotKey
+  //
+  //
+
+  HotKey.MAX_ROWS = 10;
+
+  HotKey.MAX_COLS = 1;
+
+  HotKey.prototype = Object.create(Sprite.prototype);
+  HotKey.prototype.constructor = HotKey;
+
+  HotKey.ITEM_STACK = [];
+
+  HotKey.addItemHandler = function(func) {
+  };
+
+  HotKey.prototype.initialize = function() {
+    Sprite.prototype.initialize.apply(this, arguments);
+  };
+
+  HotKey.prototype.createToolTip = function () {
+  };
+
+  HotKey.prototype.setToolTip = function (x, y, item) {
+  };
+
+  HotKey.prototype.invisibleToolTip = function () {
+  };
+
+  HotKey.prototype.visibleToolTip = function () {
+  };
+
+  HotKey.prototype.update = function() {
+    Sprite.prototype.update.call(this);
+  };
+
+  HotKey.prototype.executeHandler = function () {
+  };
+
+  HotKey.prototype.addWindow = function(object) {
+  };
+
+  HotKey.prototype.createAllItemWindow = function () {
+  };
+
+  HotKey.prototype.createWindow = function(i, w, padding, startX, startY) {
+  };
+
+  HotKey.prototype.isItem = function (__item) {
+  };
+
+  HotKey.prototype.autoDectectedItem = function() {
+  };
+
+  HotKey.prototype.loadDectectedItem = function(index) {
+  };
+
+  HotKey.prototype.addItem = function (item, index) {
+  };
+
+  HotKey.prototype.refreshHotKey = function () {
+  };
+
+  //----------------------------------------------------------------------------
+  // Input
+  //
+  //
+
   var __LocalTouchInput = {'x': 0, 'y': 0 };
   __LocalTouchInput.set = function(x, y) {
     __LocalTouchInput.x = x;
     __LocalTouchInput.y = y;
   }
 
-  //----------------------------------------------------------------------------
-  // Input
-  //
-  //
   for(var i = 0, index, key; i < 10; i++) {
     index = 48 + i;
     key = "Digit" + i;
@@ -66,7 +131,6 @@ RS.HotKey = RS.HotKey || {};
   Window_ItemAreas.prototype = Object.create(Window_Base.prototype);
   Window_ItemAreas.prototype.constructor = Window_ItemAreas;
 
-  // 초기화
   Window_ItemAreas.prototype.initialize = function (index) {
     Window_Base.prototype.initialize.call(this, 0, 0, Window_ItemAreas.WIDTH, Window_ItemAreas.HEIGHT);
     this._item = null;
@@ -80,27 +144,22 @@ RS.HotKey = RS.HotKey || {};
     this._windowFrameSprite.visible = false;
   };
 
-  // 아이템 획득
   Window_ItemAreas.prototype.item = function () {
     if(!!this._item) return this._item;
   };
 
-  // 기본 텍스트 사이즈
   Window_ItemAreas.prototype.standardFontSize = function() {
     return 14;
   };
 
-  // 텍스트 간격
   Window_ItemAreas.prototype.textPadding = function() {
     return 2;
   };
 
-  // 기본 간격
   Window_ItemAreas.prototype.standardPadding = function() {
     return 8;
   };
 
-  // 아이템 설정
   Window_ItemAreas.prototype.setItem = function(item) {
     if (this._item !== item) {
         this._item = item;
@@ -110,14 +169,12 @@ RS.HotKey = RS.HotKey || {};
     }
   };
 
-  // 아이템 상태(갯수, 활성화 상태) 설정
   Window_ItemAreas.prototype.setStatus = function (item) {
     this._status = {};
     this._status.count = $gameParty.numItems(item);
     this._status.enabled = this.isEnabled();
   };
 
-  // 상태를 초기화합니다
   Window_ItemAreas.prototype.initStatus = function(item) {
     if(!item) return false;
     this._status = {};
@@ -128,12 +185,10 @@ RS.HotKey = RS.HotKey || {};
   // 새로고침
   Window_ItemAreas.prototype.isRefresh = function () {
 
-    // 상태가 초기화되지 않았다면 실패
     if(!this._status) {
       return false;
     }
 
-    // 정보가 다르면 새로고침
     if( (this._status.count !== $gameParty.numItems(this._item)) ||
         (this._status.enabled !== this.isEnabled()) ) {
           this.initStatus();
@@ -147,7 +202,6 @@ RS.HotKey = RS.HotKey || {};
 
   };
 
-  // 키입력 (0 ~ 9)
   Window_ItemAreas.prototype.isTriggered = function () {
     if(Input.isTriggered("Digit%1".format(this._keyName)) ) {
       this.setBlueTone();
@@ -156,18 +210,15 @@ RS.HotKey = RS.HotKey || {};
     }
   };
 
-  // 아이템을 비웁니다
   Window_ItemAreas.prototype.setEmpty = function () {
     this._item = null;
     this._status = null;
   };
 
-  // 아이템이 텅 빈 상태인지 확인합니다
   Window_ItemAreas.prototype.isEmpty = function () {
     return !!this.item;
   };
 
-  // 아이템을 소비합니다.
   Window_ItemAreas.prototype.consumeItem = function () {
     if(this.isEnabled()) {
       var target = $gameParty.members()[0];
@@ -186,7 +237,6 @@ RS.HotKey = RS.HotKey || {};
     }
   };
 
-  // 아이템을 소비하는 대상(타겟)을 설정합니다
   Window_ItemAreas.prototype.itemTargetActors = function () {
     var target = $gameParty.members()[0];
     var action = new Game_Action(target);
@@ -200,14 +250,12 @@ RS.HotKey = RS.HotKey || {};
     }
   };
 
-  // 터치 영역을 획득합니다
   Window_ItemAreas.prototype.isTouchedInsideFrame = function() {
       var x = this.canvasToLocalX(__LocalTouchInput.x);
       var y = this.canvasToLocalY(__LocalTouchInput.y);
       return x >= 0 && y >= 0 && x < this.width && y < this.height;
   };
 
-  // 마우스가 위에 올라와있는지 확인합니다
   Window_ItemAreas.prototype.isOver = function () {
     if(this.isTouchedInsideFrame()) {
       if(TouchInput.isPressed()) {
@@ -233,23 +281,19 @@ RS.HotKey = RS.HotKey || {};
     }
   };
 
-  // 톤을 변경합니다.
   Window_ItemAreas.prototype.setBlueTone = function () {
     this.setTone(10, 10, 255);
   };
 
-  // 톤을 변경합니다.
   Window_ItemAreas.prototype.setRedTone = function () {
     this.setTone(255, 10, 10);
   };
 
-  // 원래의 톤으로 되돌립니다.
   Window_ItemAreas.prototype.setOriginTone = function () {
     var tone = $gameSystem.windowTone();
     this.setTone(tone[0], tone[1], tone[2]);
   };
 
-  // 업데이트
   Window_ItemAreas.prototype.update = function() {
     Window_Base.prototype.update.call(this);
     this.isRefresh();
@@ -257,7 +301,6 @@ RS.HotKey = RS.HotKey || {};
     this.isOver();
   }
 
-  // 다시 그립니다
   Window_ItemAreas.prototype.refresh = function() {
     this.contents.clear();
     this.drawText(this._keyName, 0, 0 - 8, this.width - this.textWidth('00'), 'left');
@@ -266,12 +309,10 @@ RS.HotKey = RS.HotKey || {};
     }
   };
 
-  // 아이템을 사용할 수 있는지 없는지 여부를 반환합니다.
   Window_ItemAreas.prototype.isEnabled = function() {
     return $gameParty.canUse(this._item) || false;
   };
 
-  // 아이템 하나의 영역을 획득합니다.
   Window_ItemAreas.prototype.itemRect = function () {
     var w = Window_ItemAreas.WIDTH;
     var h = Window_ItemAreas.HEIGHT;
@@ -279,7 +320,6 @@ RS.HotKey = RS.HotKey || {};
     return rect;
   };
 
-  // 아이템의 이름 영역을 그립니다.
   Window_ItemAreas.prototype.drawItemName = function(item, x, y, width) {
     width = width || Window_ItemAreas.WIDTH;
     if (item) {
@@ -290,29 +330,22 @@ RS.HotKey = RS.HotKey || {};
     }
   };
 
-  // 아이템을 그립니다.
   Window_ItemAreas.prototype.drawItem = function() {
     if (this._item) {
         var numberWidth = this.numberWidth();
         var rect = this.itemRect();
         rect.width -= this.textPadding();
-        // 투명도 설정
         this.changePaintOpacity(this.isEnabled());
-        // 아이템 이름
         this.drawItemName(this._item, rect.x, rect.y, rect.width - numberWidth);
-        // 아이템 갯수
         this.drawItemNumber(this._item, rect.x, rect.y, rect.width);
-        // 투명도 설정
         this.changePaintOpacity(1);
     }
   };
 
-  // 아이템 갯수 텍스트의 폭
   Window_ItemAreas.prototype.numberWidth = function() {
     return this.textWidth('000');
   };
 
-  // 아이템 갯수를 그립니다
   Window_ItemAreas.prototype.drawItemNumber = function(item, x, y, width) {
     var ny = this.contentsHeight() / 2;
     this.drawText(this._status.count, x, ny , width - this.textWidth('00'), 'right');
@@ -321,12 +354,11 @@ RS.HotKey = RS.HotKey || {};
   //----------------------------------------------------------------------------
   // Window_ToolTip
   //
-  // 툴팁을 표시하는 객체입니다.
+  //
 
   Window_ToolTip.prototype = Object.create(Window_Base.prototype);
   Window_ToolTip.prototype.constructor = Window_ToolTip;
 
-  // 초기화
   Window_ToolTip.prototype.initialize = function(numLines) {
       var width = 240;
       var height = this.fittingHeight(numLines || 4);
@@ -335,7 +367,6 @@ RS.HotKey = RS.HotKey || {};
       this._windowFrameSprite.visible = false;
   };
 
-  // 기본적으로 표시되는 텍스트입니다.
   Window_ToolTip.prototype.defaultText = function(text) {
     var text = ["\\c[11]Name\\c[0] : %1   \\I[%4] ",
                 "\\c[11]Price\\c[0] : %2\\G",
@@ -343,7 +374,6 @@ RS.HotKey = RS.HotKey || {};
     return text;
   };
 
-  // 무기일 경우 표시되는 텍스트입니다.
   Window_ToolTip.prototype.weaponText = function(text) {
     var text = ["\\c[11]Name\\c[0] : %1   \\I[%12] ",
                 "\\c[11]Price\\c[0] : %2\\G",
@@ -355,27 +385,22 @@ RS.HotKey = RS.HotKey || {};
     return text;
   };
 
-  // 기본 텍스트의 크기
   Window_ToolTip.prototype.standardFontSize = function() {
       return 14;
   };
 
-  // 라인의 크기
   Window_ToolTip.prototype.lineHeight = function() {
       return 36;
   };
 
-  // 간격
   Window_ToolTip.prototype.standardPadding = function() {
       return 6;
   };
 
-  // 텍스트 간격
   Window_ToolTip.prototype.textPadding = function() {
       return 6;
   };
 
-  // 텍스트를 설정합니다.
   Window_ToolTip.prototype.setText = function(text) {
       if (this._text !== text) {
           this._text = text;
@@ -383,12 +408,10 @@ RS.HotKey = RS.HotKey || {};
       }
   };
 
-  // 비어있는 텍스트를 설정합니다.
   Window_ToolTip.prototype.clear = function() {
       this.setText('');
   };
 
-  // 아이템을 설정합니다.
   Window_ToolTip.prototype.setItem = function(item) {
     var text = '';
     if(item) {
@@ -406,11 +429,9 @@ RS.HotKey = RS.HotKey || {};
           item.description, item.iconIndex);
       }
     }
-    // 아이템이 있으면 텍스트, 없으면 비어있는 텍스트
     this.setText(item ? text : '');
   };
 
-  // 설명이 폭을 넘어가면 자동으로 줄바꿈을 합니다.
   var alias_Window_ToolTip_processNormalCharacter = Window_ToolTip.prototype.processNormalCharacter;
   Window_ToolTip.prototype.processNormalCharacter = function(textState) {
       var w = this.textWidth(textState.text[textState.index]);
@@ -421,12 +442,10 @@ RS.HotKey = RS.HotKey || {};
       alias_Window_ToolTip_processNormalCharacter.call(this,textState);
   };
 
-  // 텍스트의 정확한 폭을 구합니다.
   Window_ToolTip.prototype.textWidthEx = function(text) {
     return this.drawTextEx.call(this, text, 0, this.contents.height);
   };
 
-  // 다시 그립니다.
   Window_ToolTip.prototype.refresh = function() {
       this.contents.clear();
       this.drawTextEx(this._text, this.textPadding(), 0);
@@ -435,219 +454,11 @@ RS.HotKey = RS.HotKey || {};
   //----------------------------------------------------------------------------
   // Game_System
   //
-  // 게임 정보를 저장합니다.
+  //
   var alias_Game_System_initialize = Game_System.prototype.initialize;
   Game_System.prototype.initialize = function() {
     alias_Game_System_initialize.call(this);
-    this._hotKeyItem = [];
   }
-
-  //----------------------------------------------------------------------------
-  // HotKey
-  //
-  //
-
-  // 10열
-  HotKey.MAX_ROWS = 10;
-
-  // 1행
-  HotKey.MAX_COLS = 1;
-
-  HotKey.prototype = Object.create(Sprite.prototype);
-  HotKey.prototype.constructor = HotKey;
-
-  // 아이템 스택
-  HotKey.ITEM_STACK = [];
-
-  // (static) 아이템 스택에 아이템을 추가합니다.
-  HotKey.addItemHandler = function(func) {
-    // HotKey.ITEM_STACK.push(func);
-  };
-
-  // 초기화
-  HotKey.prototype.initialize = function() {
-    Sprite.prototype.initialize.apply(this, arguments);
-
-    // 화면 영역
-    this.setFrame(0, 0, Graphics.boxWidth, Graphics.boxHeight);
-
-    // 윈도우 레이어 생성
-    this._windowLayer = new WindowLayer();
-    this._windowLayer.move(0, 0, Graphics.boxWidth, Graphics.boxHeight);
-    this.addChild(this._windowLayer);
-
-    // 모든 아이템 윈도우 생성
-    this.createAllItemWindow();
-
-    // 아이템 툴팁 생성
-    this.createToolTip();
-
-    // 셀프 포인터
-    RS.HotKey = this;
-
-    // 카운터
-    this._stayCount = 0;
-
-  };
-
-  // 툴팁 윈도우를 생성합니다.
-  HotKey.prototype.createToolTip = function () {
-    //
-    // this._toolTipWindow = new Window_ToolTip(5);
-    // this.invisibleToolTip();
-    // this.addChild(this._toolTipWindow);
-    //
-  };
-
-  // 툴팁을 설정합니다.
-  HotKey.prototype.setToolTip = function (x, y, item) {
-    // var width = this._toolTipWindow.width;
-    // var height = this._toolTipWindow.height;
-    // this._toolTipWindow.setItem(item);
-    // this._toolTipWindow.move(x, y, width, height);
-    // this.visibleToolTip();
-  }
-
-  // 툴팁을 보이지 않게 합니다.
-  HotKey.prototype.invisibleToolTip = function () {
-    // this._toolTipWindow.visible = false;
-  }
-
-  // 툴팁을 보이게 합니다.
-  HotKey.prototype.visibleToolTip = function () {
-    // this._toolTipWindow.visible = true;
-  }
-
-  // 인벤토리를 업데이트합니다.
-  HotKey.prototype.update = function() {
-    Sprite.prototype.update.call(this);
-  };
-
-  // Bug : setItem 함수가 undefined로 인식되는 버그가 있다.
-  // Bug Fix : args[1] 의 범위가 9를 초과하는 버그를 해결했다.
-  HotKey.prototype.executeHandler = function () {
-    // var self = this;
-    // for(var i = 0; i < HotKey.ITEM_STACK.length, HotKey.ITEM_STACK[i] !== undefined; i++) {
-    //   var args = HotKey.ITEM_STACK[i]();
-    //   if(args) {
-    //     if(this.isItem(args[0])) {
-    //       self.addItem(args[0], args[1].clamp(0, 9));
-    //     }
-    //   }
-    // }
-    // HotKey.ITEM_STACK = [];
-  };
-
-  // 윈도우를 추가합니다.
-  HotKey.prototype.addWindow = function(object) {
-    // if(this._windowLayer && object) {
-    //   this._windowLayer.addChild(object);
-    // }
-  };
-
-  // 모든 아이템 윈도우를 생성합니다.
-  HotKey.prototype.createAllItemWindow = function () {
-    try {
-      // var startX, w = 68 ,
-      //     h = 68,
-      //     startY,
-      //     localItem,
-      //     lastItem,
-      //     padding = 10,
-      //     self = this,
-      //     dectectedItem;
-      //
-      // startX = Graphics.boxWidth / 2 - (w * HotKey.MAX_ROWS) / 2;
-      // startY = Graphics.boxHeight - h - padding;
-
-      // this.autoDectectedItem();
-      //
-      // for(var i = 0; i < HotKey.MAX_ROWS; i++) {
-      //   localItem = this.createWindow(i, w, padding, startX, startY);
-      //   dectectedItem = this.loadDectectedItem(i);
-      //   if(dectectedItem) {
-      //     localItem.setItem(dectectedItem.item);
-      //   }
-      //   self.addWindow(localItem);
-      // }
-      //
-      // this.refreshHotKey();
-    } catch(e) {
-      throw new Error(e.message);
-    }
-  };
-
-  // 아이템 윈도우를 생성합니다.
-  HotKey.prototype.createWindow = function(i, w, padding, startX, startY) {
-    // var equ = (i * w) + padding;
-    // var localItem = new Window_ItemAreas((i + 1) % HotKey.MAX_ROWS);
-    // localItem.x = startX + equ;
-    // localItem.y = startY;
-    // return localItem;
-  }
-
-  // 아이템이 하나라도 있는지 확인합니다
-  HotKey.prototype.isItem = function (__item) {
-    // var includes = [];
-    // includes.push( DataManager.isItem( __item ) );
-    // includes.push( DataManager.isSkill( __item ) );
-    // includes.push( DataManager.isWeapon( __item ) );
-    // includes.push( DataManager.isArmor( __item ) );
-    // return includes.indexOf(true) !== -1;
-  };
-
-  // 아이템을 자동으로 감지하여 추가합니다.
-  HotKey.prototype.autoDectectedItem = function() {
-    // 윈도우 레이어가 생성되지 않았으면 빈 배열을 반환합니다.
-
-    // if(!this._windowLayer) return [];
-    //
-    // if(($gameSystem._hotKeyItem instanceof Array) &&
-    //     $gameSystem._hotKeyItem.length === 0) {
-    //   $gameSystem._hotKeyItem = [];
-    //   var list = this._windowLayer.children.filter(function(i) {
-    //       var __item = i.item();
-    //       return this.isItem(__item);
-    //   }, this);
-    //   for(var i of list) {
-    //     $gameSystem._hotKeyItem.push({'_index': i._index, 'item': i.item()});
-    //   }
-    // }
-    //
-    // return $gameSystem._hotKeyItem;
-  };
-
-  // 아이템을 로드합니다.
-  HotKey.prototype.loadDectectedItem = function(index) {
-    // 윈도우 레이어가 없으면 빈 배열을 반환합니다.
-
-    // if(!this._windowLayer) return [];
-    // if($gameSystem._hotKeyItem.length === 0) return [];
-    // return $gameSystem._hotKeyItem[index];
-  }
-
-  // 아이템 추가합니다.
-  HotKey.prototype.addItem = function (item, index) {
-    // if(this._windowLayer) {
-    //     var key = index;
-    //     var wnd = this._windowLayer.children.filter(function(i) {
-    //       if(i._keyName == key.toString()) {
-    //         return true;
-    //       }
-    //     }, this)[0];
-    //     if(wnd) {
-    //       wnd.setItem(item);
-    //       $gameSystem._hotKeyItem.push({'_index': wnd._index, 'item': wnd.item()});
-    //     }
-    // }
-  };
-
-  HotKey.prototype.refreshHotKey = function () {
-    // var list = $gameSystem._hotKeyItem;
-    // list.forEach(function(i) {
-    //   this.addItem(i.item, i._index);
-    // }, this);
-  };
 
   //----------------------------------------------------------------------------
   // Scene_Map
