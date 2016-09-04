c6103dda68bc402b43b5290b45937c356daf4aa4
biud436 <biud436@gmail.com>
2016-09-05 05:27:54 +0900

    .

---
+++ Laboratory/RS_StateIconFix.js
@@ -0,0 +1,27 @@
+//==============================================================================
+// RS_StateIconFix.js
+//==============================================================================
+/*:
+ * @pluindesc State Icon MAX_TEXTURE_SIZE Error fixed.
+ * @author biud436
+ */
+
+(function () {
+
+  Sprite_StateIcon.prototype.loadBitmap = function() {
+    var bitmap = ImageManager.loadSystem('IconSet');
+    var pw = Sprite_StateIcon._iconWidth;
+    var ph = Sprite_StateIcon._iconHeight;
+    this.bitmap = new Bitmap(pw, ph);
+  };
+
+  Sprite_StateIcon.prototype.updateFrame = function() {
+    var bitmap = ImageManager.loadSystem('IconSet');
+    var pw = Sprite_StateIcon._iconWidth;
+    var ph = Sprite_StateIcon._iconHeight;
+    var sx = this._iconIndex % 16 * pw;
+    var sy = Math.floor(this._iconIndex / 16) * ph;
+    this.bitmap.blt(bitmap, sx, sy, pw, ph, 0, 0);
+  };
+
+})();
