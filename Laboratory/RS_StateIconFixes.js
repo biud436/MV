69802886f951e162a3279b2730a7ac05d5643e98
biud436 <biud436@gmail.com>
2016-09-05 11:28:21 +0900

    RMMV 1.3.1 Bug Fixed

--- Laboratory/RS_StateIconFixes.js
+++ Laboratory/RS_StateIconFixes.js
@@ -12,17 +12,39 @@
   Sprite_StateIcon.prototype.initMembers = function() {
     alias_Sprite_StateIcon_initMembers.call(this);
     this._prevIconIndex = -1;
+    this._isAlias = false;
+    if(Graphics.isWebGL()) {
+      var gl = Graphics._renderer.gl;
+      this._maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
+    }
   };
 
+  var alias_Sprite_StateIcon_loadBitmap = Sprite_StateIcon.prototype.loadBitmap;
   Sprite_StateIcon.prototype.loadBitmap = function() {
     var bitmap = ImageManager.loadSystem('IconSet');
     var pw = Sprite_StateIcon._iconWidth;
     var ph = Sprite_StateIcon._iconHeight;
-    this.bitmap = new Bitmap(pw, ph);
+    if(Graphics.isWebGL()) {
+      if( (this._maxTextureSize - bitmap.height) < 0 ||
+          (this._maxTextureSize - bitmap.width) < 0
+          ) {
+            this.bitmap = new Bitmap(pw, ph);
+      } else {
+        this._isAlias = true;
+        alias_Sprite_StateIcon_loadBitmap.call(this);
+      }
+    } else {
+      this._isAlias = true;
+      alias_Sprite_StateIcon_loadBitmap.call(this);
+    }
   };
 
+  var alias_Sprite_StateIcon_updateFrame = Sprite_StateIcon.prototype.updateFrame;
   Sprite_StateIcon.prototype.updateFrame = function() {
     if(this._prevIconIndex === this._iconIndex) return;
+    if(this._isAlias) {
+      return alias_Sprite_StateIcon_updateFrame.call(this);
+    }
     this._prevIconIndex = this._iconIndex;
     var bitmap = ImageManager.loadSystem('IconSet');
     var pw = Sprite_StateIcon._iconWidth;
