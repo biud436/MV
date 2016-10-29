bd47b1f82e0de962c77d999f5db378d5707fd398
biud436 <biud436@gmail.com>
2016-10-30 05:30:54 +0900

    Optimized

--- RS_EventCreate.js
+++ RS_EventCreate.js
@@ -1,38 +1,56 @@
 /*:
  * RS_EventCreate.js
- * @plugindesc It is possible to create or copy or delete an event
- * through the plugin command.
+ * @plugindesc (v1.0.3) It is possible to create or copy or delete an event via the plugin commands.
  *
  * @author biud436
  * @since 2015.10.16
- * @version 1.3
  *
  * @help
- *
- * - Plugin Command
- * This plugin command creates a new event on currently map.
- * Event Create X Y CharacterName CharacterIndex
- *
+ * =============================================================================
+ * Plugin Command
+ * =============================================================================
  * This plugin command is copying an event.
+ * MapID : if the value is zero or - 1, the mapId will set up an id of currently map.
  * Event Copy X Y MapID EventID
  *
  * This plugin command deletes a previously created event on currently map.
+ * Note : Using this function, the performance will be reduced.
  * Event Delete EventID
  *
- * - Change Log
- * 2015.10.17 - First Release Date
- * 2016.02.19 - Fixed a bug that causes a serious problem.
- * 2016.02.23 - Fixed a bug.
- * 2016.02.24 - Fixed a bug that is initialized to the default value when the parameters were set to zero.
+ * This plugin command creates a new event on currently map.
+ * Event Create X Y CharacterName CharacterIndex
  *
+ * =============================================================================
+ * Script Calls
+ * =============================================================================
+ * RS.Event.instanceCopy(x, y, mapID, eventID, customData);
+ * customData : if its value sets to null, it will use existing event data.
+ *
+ * Note : Using this function, the performance will be reduced.
+ * RS.Event.instanceDestroy($gameMap.events().last);
+ * =============================================================================
+ * Change Log
+ * =============================================================================
+ * 2015.10.17 (v1.0.0) - First Release Date
+ * 2016.02.19 (v1.0.1) - Fixed a bug that causes a serious problem.
+ * 2016.02.23 (v1.0.2) - Fixed a bug.
+ * 2016.02.24 (v1.0.3) - Fixed a bug that is initialized to the default value when the parameters were set to zero.
+ * 2016.10.30 (v1.0.4) - Optimized.
  */
 
 var Imported = Imported || {};
 Imported.RS_EventCreate = true;
 
 var RS = RS || {};
+RS.Event = RS.Event || {};
 
-(function() {
+(function($) {
+
+  var defaultFolder = "data/Map";
+
+  //============================================================================
+  // Array
+  //============================================================================
 
   Object.defineProperty(Array.prototype, "first", {
     get: function() {
@@ -50,38 +68,71 @@ var RS = RS || {};
   Array.prototype.delete = function(deleteItem) {
     var tmp = this.filter(
       function(findValue) {
-        return findValue != deleteItem;
+        return findValue !== deleteItem;
       }
     );
     return tmp;
   };
 
-  RS.instanceCreate = function(x, y, charName, charIdx) {
+  //============================================================================
+  // RS.Event
+  //============================================================================
+
+  $.instanceCreate = function(x, y, charName, charIdx) {
+
+    // Set up the event Id.
     var eventID = $gameMap.events().length + 1;
+
+    // Set up the event name.
     var eventName = "EV" + String(eventID).padZero(3);
+
+    // Set up the custom event data.
     var event = { "id": eventID, "name": eventName, "note": "", "pages": [{ "conditions": { "actorId": 1, "actorValid": false, "itemId": 1, "itemValid": false, "selfSwitchCh": "A", "selfSwitchValid": false, "switch1Id": 1, "switch1Valid": false, "switch2Id": 1, "switch2Valid": false, "variableId": 1, "variableValid": false, "variableValue": 0 }, "directionFix": false, "image": { "tileId": 0, "characterName": charName, "direction": 2, "pattern": 1, "characterIndex": charIdx }, "list": [{ "code": 101, "indent": 0, "parameters": [charName, charIdx, 0, 2] }, { "code": 401, "indent": 0, "parameters": ["\\c[4]-TEST-\\c[0]"] }, { "code": 401, "indent": 0, "parameters": ["TEST"] }, { "code": 0, "indent": 0, "parameters": [] }], "moveFrequency": 3, "moveRoute": { "list": [{ "code": 0, "parameters": [] }], "repeat": true, "skippable": false, "wait": false }, "moveSpeed": 3, "moveType": 0, "priorityType": 1, "stepAnime": false, "through": false, "trigger": 0, "walkAnime": true }], "x": x, "y": y };
+
+    // Add a new event to the event elements of current map.
     $dataMap.events[event.id] = event;
-    return RS.instanceCopy(x, y, $gameMap.mapId(), eventID, event);
+
+    return $.instanceCopy(x, y, $gameMap.mapId(), eventID, event);
   };
 
-  RS.instanceCopy = function(x, y, mapID, eventID ) {
+  $.instanceCopy = function(x, y, mapID, eventID ) {
+    var eventData, scene;
+
+    // Check that the map ID is the same.
     if($gameMap.mapId() === mapID) {
-      var _event = new Game_Event(mapID || $gameMap.mapId(), eventID || 1);
-      _event.setPosition(x, y);
-      if(arguments[4]) _event.setCustomData(arguments[4]);
-      _event.refresh();
-      $gameMap._events.push(_event);
-      SceneManager._scene._spriteset.createCharacters();
+
+      // Create a new event.
+      var eventData = new Game_Event(mapID || $gameMap.mapId(), eventID || 1);
+
+      // Set up the position of the event itself.
+      eventData.setPosition(x, y);
+
+      // Check whether the fourth argument is used.
+      if(arguments[4]) eventData.setCustomData(arguments[4]);
+
+      // Set up the event page.
+      eventData.refresh();
+
+      // Pushed its event.
+      $gameMap._events.push(eventData);
+
+      // Check whether the user is on the map.
+      scene = SceneManager._scene;
+      if(scene instanceof Scene_Map) {
+        // TODO: Using this function, the performance will be reduced.
+        scene._spriteset.createCharacters();
+      }
+
       return $gameMap._events.last;
     } else {
-      this.getMapData(x, y, mapID, eventID);
+      return this.getMapData(x, y, mapID, eventID);
     }
   };
 
-  RS.getMapData = function(x, y, mapID, eventID) {
+  $.getMapData = function(x, y, mapID, eventID) {
       var self = this;
       var xhr = new XMLHttpRequest();
-      var url = 'data/' + "Map" + "%1".format(mapID).padZero(3) + ".json";
+      var url = defaultFolder + mapID.padZero(3) + ".json";
       xhr.open('GET', url);
       xhr.overrideMimeType('application/json');
       xhr.onload = function() {
@@ -94,24 +145,45 @@ var RS = RS || {};
           }
       }
     xhr.onerror = function() {
-      if($gameSystem.isKorean()) {
-        throw new Error("맵 데이터를 로드하는데 실패 했습니다.");
-      } else {
-        throw new Error('Failed to load: ' + DataManager._errorUrl);
-      }
+      throw new Error('Failed to load map data.');
     };
     xhr.send(null);
   };
 
-  RS.instanceDestroy = function(_event) {
-    if(_event instanceof Game_Event)
-    {
+  $.checkCharacterImage = function(imageName, func) {
+      var self = this;
+      var xhr = new XMLHttpRequest();
+      var url = '/img/characters/' + imageName;
+      xhr.open('GET', url);
+      xhr.overrideMimeType('application/json');
+      xhr.onload = function() {
+        if (xhr.status < 400) {
+          func();
+        }
+      }
+    xhr.onerror = function() {
+      throw new Error('Failed to load an image ' + imageName);
+    };
+    xhr.send(null);
+  };
+
+  $.instanceDestroy = function(_event) {
+    if(_event instanceof Game_Event) {
       $gameMap._events.forEach( function(event) {
         if(event.eventId() === _event.eventId()) {
+          // Delete an map event data.
           $gameMap._events = $gameMap._events.delete(_event);
+          delete $dataMap.events[_event.eventId()];
         }
       });
-      SceneManager._scene.stage._spriteset.createLowerLayer();
+
+      // Check whether the user is on the map.
+      var scene = SceneManager._scene;
+      if(scene instanceof Scene_Map) {
+        // TODO: Using this function, the performance will be reduced.
+        scene._spriteset.createLowerLayer();
+      }
+
     }
   };
 
@@ -122,22 +194,26 @@ var RS = RS || {};
   var alias_Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
   Game_Interpreter.prototype.pluginCommand = function(command, args) {
     alias_Game_Interpreter_pluginCommand.call(this, command, args);
-    if(command === "이벤트" || command === "Event") {
-      switch (args[0]) {
-      case '생성':
-      case 'Create':
-        RS.instanceCreate(Number(args[1]), Number(args[2]), args[3], Number(args[4]));
+    if(command === "Event") {
+      switch ( args[0].toLowerCase() ) {
+      case 'create':
+        var x = Number(args[1]).clamp(0, $gameMap.width() - 1);
+        var y = Number(args[2]).clamp(0, $gameMap.height() - 1);
+        var charName = args[3];
+        var charIdx = Number(args[4]).clamp(0, 7);
+        $.checkCharacterImage(charName, function () {
+          $.instanceCreate(x, y, charName, charIdx);
+        });
         break;
-      case '복제':
-      case 'Copy':
-        RS.instanceCopy(Number(args[1]), Number(args[2]), Number(args[3]), Number(args[4]));
+      case 'copy':
+        var mapId = Number(args[3]);
+        if(mapId <= 0) mapId = $gameMap.mapId();
+        $.instanceCopy( Number(args[1]), Number(args[2]), mapId , Number(args[4]) );
         break;
-      case '삭제':
-      case 'Delete':
-        RS.instanceDestroy( this.character(Number(args[1])) );
+      case 'delete':
+        $.instanceDestroy( this.character(Number(args[1])) );
         break;
       }
-
     }
   };
 
@@ -167,5 +243,4 @@ var RS = RS || {};
     }
   };
 
-
-})();
+})(RS.Event);
