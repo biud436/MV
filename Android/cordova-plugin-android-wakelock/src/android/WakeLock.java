2cc4d6e0599119891b8df8b7bd0cd3e05ea262c8
biud436 <biud436@gmail.com>
2016-11-04 11:12:52 +0900

    .

---
+++ Android/cordova-plugin-android-wakelock/src/android/WakeLock.java
@@ -0,0 +1,66 @@
+/*
+   Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+
+	 http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
+*/
+
+package org.apache.cordova.wakelock;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+
+public class WakeLock extends CordovaPlugin {
+
+	private PowerManager.WakeLock mWakeLock;
+
+	public WakeLock() {}
+
+  public void onDestroy() {
+		super.onDestroy();
+    this.releaseLock();
+  }
+
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals("init")) {
+			this.acquireLock( callbackContext );
+		}
+		callbackContext.success();
+		return true;
+	}
+
+	public void acquireLock(Context context) {
+	   PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+	   mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "RMMV GAME LOCK");
+	   mWakeLock.acquire();
+	}
+
+	public void releaseLock() {
+		if (mWakeLock != null) {
+			mWakeLock.release();
+			mWakeLock = null;
+		}
+	}
+
+}
