4dae6cd3427d23efb73094421610b1409ba2e08d
biud436 <biud436@gmail.com>
2016-12-29 01:11:25 +0900

    .

--- RS_WinTray.js
+++ RS_WinTray.js
@@ -3,32 +3,35 @@
  * @author biud436
  *
  * @param url
- * @desc game developer's website
+ * @desc
  * @default http://www.google.com/
  *
  * @param --- Tray Menu
  * @desc
  * @default
  *
+ * @param Open
+ * @desc
+ * @default Open
+ *
  * @param tooltip
- * @desc You can write a tooltip for the tray. When you place your
- * mouse on top of the tray's icon, the tooltip is shown.
- * @default Minimized window becomes active.
+ * @desc
+ * @default tooltip
  *
  * @param Minimize To Tray
- * @desc you can edit a contents about the 'minimize to tray'
- * @default Active window becomes minimized to tray.
+ * @desc
+ * @default To Tray
  *
  * @param Window Always On Top
- * @desc you can edit a contents about the 'Window Always On Top'
+ * @desc
  * @default Window Always On Top
  *
  * @param Show Internet
- * @desc Show developer's website
+ * @desc
  * @default Open URL
  *
  * @param Exit
- * @desc Game Exit
+ * @desc
  * @default Game Exit
  *
  * @help
@@ -36,99 +39,139 @@
  * Change Log
  * =============================================================================
  * 2016.01.21 (v1.0.0) - First Release.
+ * 2016.12.28 (v1.0.1) - Fixed a bug that the tray will remain after closing the window.
  */
 
 var Imported = Imported || {};
+var RS = RS || {};
+RS.Tray = RS.Tray || {};
+RS.Tray.Params = RS.Tray.Params || {};
 Imported.RS_WinTray = true;
 
-(function() {
+function Tray() {
+    this.initialize.apply(this, arguments);
+}
 
-  if(!Utils.isNwjs()) {
-    return false;
-  }
+(function($) {
 
-  // Private Members
-  var parameters, path, gui, win, tray, menu;
-  var _tooltip, _url, _minimize_to_tray, _show_window, _window_always_on_top;
-  var _show_internet, _exit;
+  if(typeof require === 'undefined') return false;
 
   // Tray Menu
-  parameters = PluginManager.parameters('RS_WinTray');
-  _tooltip = parameters['tooltip'];
-  _url = parameters['url'];
-  _minimize_to_tray = parameters['Minimize To Tray'];
-  _window_always_on_top = parameters['Window Always On Top'];
-  _show_internet = parameters['Show Internet'];
-  _exit = parameters['Exit'];
+  var parameters = PluginManager.parameters('RS_WinTray');
 
-  function initialize() {
-    initMembers();
-    initTray();
-    setupMenu();
+  $.menuString = {
+    'open': parameters['Open'],
+    'tooltip': parameters['tooltip'],
+    'url': parameters['url'],
+    'minimize_to_tray': parameters['Minimize To Tray'],
+    'window_always_on_top': parameters['Window Always On Top'],
+    'show_internet': parameters['Show Internet'],
+    'exit': parameters['Exit']
   }
 
-  function initMembers() {
-    path = setupPath();
-    gui = require('nw.gui');
-    win = gui.Window.get();
-    tray = new gui.Tray({icon: path + 'icon.png' });
-    menu = new gui.Menu();
-  }
+  Tray.prototype.initialize = function() {
+      this.initMembers();
+      this.initTray();
+      this.setupMenu();
+  };
 
-  // Icon Path
-  function setupPath() {
-    var path = window.location.pathname.replace(/(\/www|)\/[^\/]*$/, '/icon/');
-    if (path.match(/^\/([A-Z]\:)/)) {
-      path = path.slice(1);
-    }
-    return decodeURIComponent(path);
-  }
+  Tray.prototype.initMembers = function() {
+      var self = this;
+      var gui = require('nw.gui');
+      this._path = this.setupPath();
+      this._win = gui.Window.get();
+      this._tray = new gui.Tray({icon: self._path + 'icon.png' });
+      this._menu = new gui.Menu();
+      this._status = false;
+  };
 
-  function initTray() {
-    tray.menu = menu;
-    tray.tooltip = _tooltip;
-    tray.on('click', function() {
-      win.show();
-    });
-  }
-
-  function setupMenu() {
-
-    // Create Tray Menu
-    var menuItem = new gui.MenuItem({ type: 'checkbox', label: _minimize_to_tray});
-
-    // Add Tray Menu
-    menuItem.click = function() {
-
-      if(this.checked) {
-        win.hide();
-      } else {
-        win.show();
+  Tray.prototype.setupPath = function () {
+      var path = window.location.pathname.replace(/(\/www|)\/[^\/]*$/, '/icon/');
+      if (path.match(/^\/([A-Z]\:)/)) {
+        path = path.slice(1);
       }
-    };
+      return decodeURIComponent(path);
+  };
 
-    menu.append(menuItem);
+  Tray.prototype.initTray = function () {
+      var self = this;
+      this._tray.menu = this._menu;
+      this._tray.tooltip = $.menuString['tooltip'];
+      this._tray.on('click', function() {
+        self._menu.items[0].click();
+      });
+      this._tray.on('close', function () {
+        if(self._tray) {
+          self._tray.remove();
+          self._tray = null;
+        }
+      });
+  };
 
-    // Add the button called always on top.
-    menu.append(new gui.MenuItem({ type: 'checkbox', label: _window_always_on_top, click: function() {
-        win.setAlwaysOnTop(this.checked);
+  Tray.prototype.setupMenu = function () {
+
+    var self = this;
+    var gui = require('nw.gui');
+
+    self._menu.append(new gui.MenuItem({
+      type: 'normal',
+      label: $.menuString['open'],
+      click: function() {
+        self._win.show();
+        self._win.focus();
       }
     }));
 
-    // Add the button called show internet.
-    menu.append(new gui.MenuItem({ type: 'normal', label: _show_internet, click: function() {
+    self._menu.append(new gui.MenuItem({
+      type: 'checkbox',
+      label:  $.menuString['minimize_to_tray'],
+      click: function () {
+        if(this.checked) {
+          self._win.hide();
+        } else {
+          self._win.show();
+          self._win.focus();
+        }
+      }
+    }));
+
+    self._menu.append(new gui.MenuItem({
+      type: 'checkbox',
+      label: $.menuString['window_always_on_top'],
+      click: function() {
+        self._win.setAlwaysOnTop(this.checked);
+      }
+    }));
+
+    self._menu.append(new gui.MenuItem({
+      type: 'normal',
+      label: $.menuString['show_internet'],
+      click: function() {
         var shell = require('nw.gui').Shell;
-        shell.openExternal(_url);
+        shell.openExternal($.menuString['url']);
       }
     }));
 
-    // Add the button called exit
-    menu.append(new gui.MenuItem({ type: 'normal', label: _exit, click: function() {
-        win.close();
+    self._menu.append(new gui.MenuItem({
+      type: 'normal',
+      label: $.menuString['exit'],
+      click: function() {
+        if(self._tray) {
+          self._tray.remove();
+          self._tray = null;
+        }
+        if(self._win !== null) {
+          self._win.close(true);
+          self._win = null;
+        }
       }
     }));
+
+  };
+
+  if(!RS.Tray.system) {
+    RS.Tray.system = new Tray();
   }
 
-  initialize();
 
-})();
+})(RS.Tray.Params);
