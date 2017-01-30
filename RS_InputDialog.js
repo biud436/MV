b095c57d6a885f8656e3093dd5a4789995e618d7
biud436 <biud436@gmail.com>
2017-01-30 14:45:08 +0900

    Fixed an issue that is not working properly if the text dialog has a string to start with a number.

--- RS_InputDialog.js
+++ RS_InputDialog.js
@@ -114,6 +114,7 @@
  * - In the debug mode, It adds the result value to a log window after the text input is done.
  * 2016.12.08 (v1.1.68) - Removed the text hint window.
  * 2016.12.17 (v1.1.69) - Fixed an issue that an integer value could not be checked due to the text type issue.
+ * 2017.01.30 (v1.1.7) - Fixed an issue that is not working properly if the text dialog has a string to start with a number.
  */
 
 var Imported = Imported || {};
@@ -508,7 +509,7 @@ function Scene_InputDialog() {
     if(!this._textBox) return '';
     if( this.textBoxIsBusy() ) {
       var text = this._textBox.getText() || '';
-      if(text.match(/^([\d]+)/g)) text = Number(RegExp.$1);
+      if(text.match(/^([\d]+)$/g)) text = Number(RegExp.$1);
       $gameVariables.setValue(RS.InputDialog.Params.variableID, text);
       this._textBox.setText('');
       if(RS.InputDialog.Params.debug) {
