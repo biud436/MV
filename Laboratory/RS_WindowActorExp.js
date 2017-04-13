ce3334895571ca52762c68dcc7878a97163cabe8
biud436 <biud436@gmail.com>
2017-04-13 16:41:37 +0900

    .

--- Laboratory/RS_WindowActorExp.js
+++ Laboratory/RS_WindowActorExp.js
@@ -1,5 +1,5 @@
 /*:
- * @plugindesc (v1.0.0) This plugin allows you to clearly display a gauge bar for Exp value to status window.
+ * @plugindesc (v1.0.1) This plugin allows you to clearly display a gauge bar for Exp value to status window.
  * @author biud436
  *
  * @param Exp Gauge Color1
@@ -10,8 +10,17 @@
  * @desc Change the gauge color for exp
  * @default 11
  *
- * @help
+ * @param Padding
+ * @desc Change padding value of each line in Menu Status
+ * @default 10
  *
+ * @help
+ * =============================================================================
+ * Change log
+ * =============================================================================
+ * 2017.04.11 (v1.0.0) - First Release.
+ * 2017.04.12 (v1.0.1) - Added a new parameter can be able to set the padding
+ * value of each line in Menu Status Window.
  */
 
 var Imported = Imported || {};
@@ -24,6 +33,7 @@ Imported.RS_WindowActorExp = true;
   var parameter = PluginManager.parameters('RS_WindowActorExp');
   var colorExpGauge1 = parseInt(parameter['Exp Gauge Color1'] || 10);
   var colorExpGauge2 = parseInt(parameter['Exp Gauge Color2'] || 11);
+  var linePadding = parseInt(parameter['Padding'] || 10);
 
   //============================================================================
   // Using YEP?
@@ -75,8 +85,7 @@ Imported.RS_WindowActorExp = true;
 
   var alias_Window_MenuStatus_drawActorSimpleStatus = Window_MenuStatus.prototype.drawActorSimpleStatus;
   Window_MenuStatus.prototype.drawActorSimpleStatus = function(actor, x, y, width) {
-      alias_Window_MenuStatus_drawActorSimpleStatus.call(this, actor, x, y, width);
-      var lineHeight = this.lineHeight();
+      var lineHeight = this.lineHeight() - linePadding;
       var x2 = x + 180;
       var width2 = Math.min(200, width - 180 - this.textPadding());
       var xpad = 0;
@@ -85,6 +94,13 @@ Imported.RS_WindowActorExp = true;
         x2 = x + xpad;
         width2 = Math.max(180, width - xpad - this.textPadding());
       }
+      this.contents.fontSize = lineHeight;
+      this.drawActorName(actor, x, y);
+      this.drawActorLevel(actor, x, y + lineHeight * 1);
+      this.drawActorIcons(actor, x, y + lineHeight * 2);
+      this.drawActorClass(actor, x2, y);
+      this.drawActorHp(actor, x2, y + lineHeight * 1, width2);
+      this.drawActorMp(actor, x2, y + lineHeight * 2, width2);
       this.drawActorExp(actor, x2, y + lineHeight * 3, width2);
   };
 
