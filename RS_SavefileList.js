32125794ce778273928459b2465b4b32b3ed3de3
biud436 <biud436@gmail.com>
2017-06-09 02:09:43 +0900

    .

--- RS_SavefileList.js
+++ RS_SavefileList.js
@@ -14,6 +14,9 @@
  * @help
  * 2015.12.21 (v1.0.0) - First Release
  * 2016.07.15 (v1.0.1) - Fixed the bug and then added plugin parameters
+ * 2017.06.09 (v1.0.2) :
+ * - Added a function called 'reserveFaceImages' to Window_SavefileList
+ * - Fixed the padding value correctly
  */
 
  /*:ko
@@ -32,6 +35,9 @@
   * @help
   * 2015.12.21 (v1.0.0) - First Release
   * 2016.07.15 (v1.0.1) - Fixed the bug and then added plugin parameters
+  * 2017.06.09 (v1.0.2) :
+  * - Added a function called 'reserveFaceImages' to Window_SavefileList
+  * - Fixed the padding value correctly
   */
 
 var Imported = Imported || {};
@@ -42,6 +48,12 @@ Imported.RS_SaveFileList = true;
   var parameters = PluginManager.parameters('RS_SaveFileList');
   var isProjectTitle = Boolean(parameters['Project Title'] === 'true');
   var isPlayTime = Boolean(parameters['Play Time'] === 'true');
+
+  var alias_Window_SavefileList_initialize = Window_SavefileList.prototype.initialize;
+  Window_SavefileList.prototype.initialize = function(x, y, width, height) {
+      alias_Window_SavefileList_initialize.call(this, x, y, width, height);
+      if(Utils.RPGMAKER_VERSION >= '1.5.0') this.reserveFaceImages();
+  };
 
   Window_SavefileList.prototype.drawFace = function(faceName, faceIndex, x, y, width, height) {
       width = width || Window_Base._faceWidth;
@@ -78,7 +90,7 @@ Imported.RS_SaveFileList = true;
       if (info.characters) {
           for (var i = 0; i < info.faces.length; i++) {
               var data = info.faces[i];
-              this.drawFace(data[0], data[1], x + i * 78, y + this.standardPadding(), 144, 144);
+              this.drawFace(data[0], data[1], x + i * 78, y + this.textPadding() * 2, 144, 144);
           }
       }
   };
