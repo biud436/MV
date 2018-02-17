b26b13f48ce4af609c902ae981257d179bf2de14
biud436 <biud436@gmail.com>
2018-02-18 03:59:55 +0900

    .

--- Hangul/RS_HangulBitmapText.js
+++ Hangul/RS_HangulBitmapText.js
@@ -17,4 +17,40 @@ Imported.RS_HangulBitmapText = true;
     init = true;
   }
 
+  Bitmap.snapFast = function(stage, maxWidth, lineHeight) {
+      var width = maxWidth;
+      var height = lineHeight;
+      var bitmap = new Bitmap(width, height);
+      var context = bitmap._context;
+      var renderTexture = PIXI.RenderTexture.create(width, height);
+      if (stage) {
+          Graphics._renderer.render(stage, renderTexture);
+          stage.worldTransform.identity();
+          var canvas = null;
+          if (Graphics.isWebGL()) {
+              canvas = Graphics._renderer.extract.canvas(renderTexture);
+          } else {
+              canvas = renderTexture.baseTexture._canvasRenderTarget.canvas;
+          }
+          context.drawImage(canvas, 0, 0);
+      } else {
+
+      }
+      renderTexture.destroy({ destroyBase: true });
+      bitmap._setDirty();
+      return bitmap;
+  };
+
+
+  Bitmap.prototype.drawText = function(text, x, y, maxWidth, lineHeight, align) {
+    var fontSize = this.fontSize;
+    align = align || "left";
+    var bitmapFontText = new PIXI.extras.BitmapText(text, {
+        font: '%1px 나눔고딕'.format(fontSize),
+        align: align
+      });
+    var bitmap = Bitmap.snapFast(bitmapFontText, maxWidth, fontSize);
+    this.blt(bitmap, 0, 0, bitmap.width, bitmap.height, x, y, bitmap.width, bitmap.height);
+  };
+
 })();
