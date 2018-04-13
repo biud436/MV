d432a871d7b1d13bbeda50cf63019bf0543e4c7d
biud436 <biud436@gmail.com>
2018-04-13 23:51:13 +0900

    ...

--- RS_PictureTool.js
+++ RS_PictureTool.js
@@ -6,9 +6,13 @@
  * 스크립트 호출
  * =============================================================================
  *
- * 이벤트와 그림이 충돌했는 지를 확인하고 이벤트를 실행합니다 :
+ * 특정 그림과 특정 이벤트 eventId가 충돌하면 eventId를 실행합니다.
  *
- *    RS.PictureTool.runEventFromPictureBound(picId, eventId);
+ *    RS.PictureTool.runEventCollideWithPicture(picId, eventId);
+ *
+ * 그림과 플레이어가 충돌하면 특정 이벤트를 실행합니다.
+ *
+ *    RS.PictureTool.runEventCollideWithPlayer(picId, eventId);
  *
  * =============================================================================
  * 버전 로그
@@ -21,6 +25,8 @@ RS.PictureTool = RS.PictureTool || {};
 
 
 (function ($) {
+
+  $.Params = $.Params || {};
 
   $.isMap = function () {
     return SceneManager._scene instanceof Scene_Map;
@@ -51,9 +57,9 @@ RS.PictureTool = RS.PictureTool || {};
   };
 
   /**
-   * 영역 안에 있는 이벤트 실행
+   * 특정 그림과 특정 이벤트 eventId가 충돌하면 eventId를 실행합니다.
    */
-  $.runEventFromPictureBound = function (picId, eventId) {
+  $.runEventCollideWithPicture = function (picId, eventId) {
     var pic = $.findPictureBound(picId);
     if(!pic) return false;
 
@@ -68,6 +74,41 @@ RS.PictureTool = RS.PictureTool || {};
 
     var sx = e.screenX();
     var sy = e.screenY();
+
+    if( (sx < px) ||
+    (sx > pw) ||
+    (sy < py) ||
+    (sy > ph)) {
+      return false;
+    }
+
+    if(!$gameMap.isEventRunning()) {
+      e.start();
+    }
+
+    return true;
+
+  };
+
+
+  /**
+   * 그림과 플레이어가 충돌하면 특정 이벤트를 실행합니다.
+   */
+  $.runEventCollideWithPlayer = function (picId, eventId) {
+    var pic = $.findPictureBound(picId);
+    if(!pic) return false;
+
+    var px = pic.x;
+    var py = pic.y;
+    var pw = pic.x + pic.width;
+    var ph = pic.y + pic.height;
+
+    var e = $gameMap.event(eventId);
+
+    if(!e) return false;
+
+    var sx = $gamePlayer.screenX();
+    var sy = $gamePlayer.screenY();
 
     if( (sx < px) ||
     (sx > pw) ||
