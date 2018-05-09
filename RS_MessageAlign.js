3f3b71c679a0410dc4d00c905cceb899843d5160
biud436 <biud436@gmail.com>
2018-05-09 22:47:52 +0900

    fixed

--- RS_MessageAlign.js
+++ RS_MessageAlign.js
@@ -26,7 +26,7 @@
  * 2017.06.25 (v1.0.1) - Fixed an issue that resets a font setting at each line
  * 2017.07.23 (v1.0.2) - Fixed a bug that the alignment is not processing in a newly line
  * 2017.10.13 (v1.0.3) - Fixed the class name.
- * 2018.05.09 (v1.0.4) - Fixed the 
+ * 2018.05.09 (v1.0.4) - Added a variable that stores a last alignment value.
  */
 
 
