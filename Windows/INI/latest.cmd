13b4e89ec96d3acf97884dec2ebd7079a5e10836
biud436 <biud436@gmail.com>
2018-06-04 11:17:07 +0900

    listens for arguments being inserted

--- Windows/INI/latest.cmd
+++ Windows/INI/latest.cmd
@@ -3,10 +3,25 @@
 
 cmd /c npm install
 
-cmd /c nw-gyp configure --target="0.30.2" --arch=x64
-cmd /c nw-gyp build --target="0.30.2" --arch=x64
-cmd /c nw-gyp configure --target="0.30.2" --arch=ia32
-cmd /c nw-gyp build --target="0.30.2" --arch=ia32
+REM set FILENAME=%~f0
+set ARGS1=%1
+
+if "%ARGS1%" == ""  (
+    set ARGS1="0.30.2"
+)
+
+cmd /c nw-gyp configure --target=%ARGS1% --arch=x64
+cmd /c nw-gyp build --target=%ARGS1% --arch=x64
+cmd /c nw-gyp configure --target=%ARGS1% --arch=ia32
+cmd /c nw-gyp build --target=%ARGS1% --arch=ia32
+
+REM if "%ARGS1%" == "0.30.2"  (
+REM     set ARGS1="0.12.3"
+REM ) else (
+REM     set ARGS1="0.30.2"
+REM )
+
+REM FILENAME ARGS1
 
 @ENDLOCAL
 GOTO :EOF
