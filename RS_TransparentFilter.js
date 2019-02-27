ef8f8ee0fc191403e0c04c44e2b49dc3786388a9
biud436 <biud436@gmail.com>
2019-02-27 17:12:17 +0900

    2019.02.27 (v1.0.1) : Added the Sprite_Actor and Sprite_Enemy

--- RS_TransparentFilter.js
+++ RS_TransparentFilter.js
@@ -34,6 +34,8 @@
  * Change Log
  * ================================================================
  * 2019.02.21 (v1.0.0) - First Release.
+ * 2019.02.27 (v1.0.1) :
+ * - Added the Sprite_Actor and Sprite_Enemy
  */
 
 var Imported = Imported || {};
@@ -74,44 +76,6 @@ RS.TransparentFilter = RS.TransparentFilter || {};
           try { return $.jsonParse(v); } catch (e) { return v; }
         });
         return retData;
-    };
-
-    $.parseColorData = function(data) {
-        var ret;
-
-        var color = data;
-        var maxValue = 255.0;
-        var r = parseInt(data["@red"]) / maxValue;
-        var g = parseInt(data["@green"]) / maxValue;
-        var b = parseInt(data["@blue"]) / maxValue;
-        var a = parseInt(data["@alpha"]) / maxValue;
-        ret = [r, g, b, a].map(function(i) { return i * 1.0});    
-        
-        return ret;
-    }
-
-    $.getTransparentColor = function(path, filename) {
-        var imageGruop = $.Params.TransparentKey["imagegroup"];
-        var items = imageGruop["object"];
-        var name = `${filename}.png`;
-        var data = [];
-        var ret = [1.0, 1.0, 1.0, 1.0];
-
-        if(Array.isArray(items)) {
-            data = items.filter(function(i) {
-                return i["@path"] == path && i["@name"] == name;
-            }, this);
-
-            if(data.length > 0) {
-                ret = $.parseColorData(data[0]);
-            }            
-        } else {
-            data = items;
-            ret = $.parseColorData(data);
-        }
-
-        return ret;
-
     };
 
     //============================================================================
@@ -159,7 +123,7 @@ RS.TransparentFilter = RS.TransparentFilter || {};
 
         PIXI.Filter.call( this, vertexSrc, fragmentSrc );
 
-        this.uniforms.alphaColor = [1.0, 1.0, 1.0, 1.0];
+        this.uniforms.alphaColor = new Float32Array([1.0, 1.0, 1.0, 1.0]);
 
     };
 
@@ -176,7 +140,7 @@ RS.TransparentFilter = RS.TransparentFilter || {};
                 this.filters = [];
             }
 
-            this._alphaFilter.uniforms.alphaColor = color;
+            this._alphaFilter.uniforms.alphaColor = new Float32Array(color);
             this.filters = [this._alphaFilter].concat(this.filters);
 
         } else {
@@ -192,6 +156,161 @@ RS.TransparentFilter = RS.TransparentFilter || {};
         }
 
     };
+
+    $.parseColorData = function(data) {
+        var ret;
+
+        var color = data;
+        var maxValue = 255.0;
+        var r = parseInt(data["@red"]) / maxValue;
+        var g = parseInt(data["@green"]) / maxValue;
+        var b = parseInt(data["@blue"]) / maxValue;
+        var a = parseInt(data["@alpha"]) / maxValue;
+        ret = [r, g, b, a].map(function(i) { return i * 1.0});    
+        
+        return ret;
+    }
+
+    $.getTransparentColor = function(path, filename) {
+        var imageGruop = $.Params.TransparentKey["imagegroup"];
+        var items = imageGruop["object"];
+        var name = `${filename}.png`;
+        var data = [];
+        var ret = [1.0, 1.0, 1.0, 1.0];
+
+        if(Array.isArray(items)) {
+            data = items.filter(function(i) {
+                return i["@path"] == path && i["@name"] == name;
+            }, this);
+
+            if(data.length > 0) {
+                ret = $.parseColorData(data[0]);
+            }            
+        } else {
+            data = items;
+            ret = $.parseColorData(data);
+        }
+
+        return ret;
+
+    };
+
+    /**
+     * @param {Number} gamma The default value is to 2.2
+     */
+    $.gamma = function(color, gamma) {
+
+        var gammaCorrection = 1.0 / gamma;
+        var red = color[0];
+        var green = color[1];
+        var blue = color[2];
+        var newRed = 255 * (red / 255.0) ^ gammaCorrection;
+        var newGreen = 255 * (green / 255.0) ^ gammaCorrection;
+        var newBlue = 255 * (blue / 255.0) ^ gammaCorrection;
+        
+        return [newRed / 255.0, newGreen / 255.0, newBlue / 255.0, color[3]];
+
+    };
+
+    /**
+     * refer to Bitmap.prototype.rotateHue
+     * @param {Number} offset The hue offset in 360 degrees
+     */
+    $.rotateHue = function(offset) {
+
+        var isValid = this._colorMatrixFilter;
+
+        if(!isValid) {
+            this._colorMatrixFilter = new PIXI.filters.ColorMatrixFilter();
+
+            if(!this.filters) {
+                this.filters = [];
+            }
+
+            this._colorMatrixFilter.hue(offset);
+
+            // this._alphaFilter.uniforms.alphaColor = new Float32Array(color);
+            this.filters = this.filters.concat([this._colorMatrixFilter]);
+
+        } else {
+
+            if(!this.filters) {
+            this.filters = [];
+            }      
+
+            this.filters = this.filters.filter(function(filter) {
+                return filter !== isValid;
+            }, this);
+
+        }        
+        
+    };
+
+    //============================================================================
+    // Sprite_Actor
+    //============================================================================  
+
+    Sprite_Actor.prototype.applyTransparentFilter = function() {
+        if(!this._battler) return;
+        if(!this._battler.isSpriteVisible()) return;
+
+        var name = this._actor.battlerName();
+        if (this._battlerName !== name) {
+            if(name != "" && !this._alphaFilter) {
+                var color = $.getTransparentColor("img/sv_actors", name);
+                RS.TransparentFilter.addFilter.call(this, color);
+            }
+        }
+    };
+
+    var alias_Sprite_Actor_updateBitmap = Sprite_Actor.prototype.updateBitmap;
+    Sprite_Actor.prototype.updateBitmap = function() {
+        this.applyTransparentFilter();
+        alias_Sprite_Actor_updateBitmap.call(this);
+    };
+    
+    //============================================================================
+    // Sprite_Enemy
+    //============================================================================      
+
+    Sprite_Enemy.prototype.applyTransparentFilter = function() {
+        var name = this._enemy.battlerName();
+        var hue = this._enemy.battlerHue();
+        if (this._battlerName !== name || this._battlerHue !== hue) {
+            if(name != "" && !this._alphaFilter) {
+                var color;
+                if ($gameSystem.isSideView()) {
+                    color = $.getTransparentColor("img/sv_enemies", name);
+                } else {
+                    color = $.getTransparentColor("img/enemies", name);
+                }
+                
+                // Hue value is number 0 and 360. 
+                // its default value is to 0.
+                if(hue > 0) {
+                    $.rotateHue.call(this, hue);
+                }
+
+                RS.TransparentFilter.addFilter.call(this, color);
+            }
+        };
+    };
+
+    var alias_Sprite_Enemy_loadBitmap = Sprite_Enemy.prototype.loadBitmap
+    Sprite_Enemy.prototype.loadBitmap = function(name, hue) {
+
+        // if the hud is 0.0 or above, it must set the 0.0 point.
+        // because is that the filter must do a hue rotation in my filter.
+        hue = 0.0;
+        
+        alias_Sprite_Enemy_loadBitmap.call(this, name, hue);
+    };
+    
+    var alias_Sprite_Enemy_updateBitmap = Sprite_Enemy.prototype.updateBitmap;
+    Sprite_Enemy.prototype.updateBitmap = function() {
+        this.applyTransparentFilter();
+        alias_Sprite_Enemy_updateBitmap.call(this);
+    };    
 
     //============================================================================
     // Sprite_Character
@@ -239,7 +358,6 @@ RS.TransparentFilter = RS.TransparentFilter || {};
         this.applyTransparentFilter();
         alias_Sprite_Picture_updateBitmap.call(this);
     };    
-
 
     $.loadTransparentKey();
 
