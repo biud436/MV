6f05f186d331f79dd523fcd08d707cb9e6000d66
biud436 <biud436@gmail.com>
2019-03-09 16:29:41 +0900

    precision lowp float.

--- RS_TransparentFilter.js
+++ RS_TransparentFilter.js
@@ -98,6 +98,8 @@ RS.TransparentFilter = RS.TransparentFilter || {};
         `;
 
         var fragmentSrc = `
+        precision lowp float;
+
         varying vec2 vTextureCoord;
 
         uniform sampler2D uSampler;
