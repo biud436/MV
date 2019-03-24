5b6dca1038f11e340508d93b26d0840a59c1312a
biud436 <biud436@gmail.com>
2019-03-24 17:35:03 +0900

    .

--- RS_AnimataionVisibility.js
+++ RS_AnimataionVisibility.js
@@ -1,19 +1,42 @@
 /*:
- * @plugindesc <RS_AnimataionVisibility>
+ * @plugindesc This plugin allows you to remove the animation on the screen when the character is deactivated. <RS_AnimataionVisibility>
  * @author biud436
  * @help
- * =====================================================
+ * ================================================================
+ * Introduction
+ * ================================================================
+ * In case of the RPG Maker MV, 
+ * The Animation will be played until the last cell even if the character doesn't show up anymore on the screen.
+ * This plugin allows you to remove the animation on the screen when the character is deactivated.
+ * 
+ * ◆ Animation Stop Condition
+ *   - erased event.
+ *   - if the event execution conditions are not met.
+ *   - empty graphic.
+ * 
+ * ================================================================
  * Version Log
- * =====================================================
+ * ================================================================
  * 2019.03.24 (v1.0.0) - First Release.
  */
 /*:ko
  * @plugindesc 애니메이션을 바로 정지시킵니다. <RS_AnimataionVisibility>
  * @author biud436
  * @help
- * =====================================================
+ * ================================================================
+ * 소개
+ * ================================================================
+ * RPG Maker MV의 경우, 캐릭터가 화면에서 보이지 않아도 애니메이션이 끝까지 재생됩니다.
+ * 이 플러그인은 캐릭터가 화면에서 보이지 않으면 애니메이션을 바로 정지시킵니다.
+ * 
+ *  ◆ 정지 조건​
+ *    - 이벤트 일시 삭제
+ *    - 이벤트 실행 조건이 충족하지 않을 때 (변수, 스위치, 셀프 스위치)
+ *    - 캐릭터 그래픽이 비어있을 때
+ * 
+ * ================================================================
  * Version Log
- * =====================================================
+ * ================================================================
  * 2019.03.24 (v1.0.0) - First Release.
  */
  
