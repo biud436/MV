2b3c991fd2fcbb82b2aab15cbf362e75e625fb8c
biud436 <biud436@gmail.com>
2019-07-18 16:39:28 +0900

    added some description...

--- RS_FaceAnimation.js
+++ RS_FaceAnimation.js
@@ -4,6 +4,175 @@
  * 
  * @param Set Animation Face
  * @type struct<AnimationFace>[]
+ * @desc Set the option data for animated face
+ * @default ["{\"id\":\"blink\",\"x\":\"0\",\"y\":\"0\",\"width\":\"144\",\"height\":\"144\",\"cols\":\"4\",\"maxFrames\":\"3\",\"delay\":\"5.00\",\"looping\":\"true\"}"]
+ * 
+ * @help
+ * ================================================================
+ * Preparing for face data
+ * ================================================================
+ * In this section, you'll learn how to define a custom animated face data.
+ * 
+ * You can show up a lot of emotions such as an idle, run, sad, blink, 
+ * run, die, and so on.
+ * 
+ * In the plugin parameter called 'Set Animation Face', 
+ * You need to define a new custom face data for your own purposes.
+ * 
+ * ** id value :
+ * When the message window indicates a face, it must detect whether the face 
+ * is an animated type, so it need to a specific value. 
+ * it is the ID value of the animated face.
+ * 
+ * You can define the ID value is to string type such as idle, blink, die.
+ * 
+ * ** x and y values :
+ * Next, You need to set the x and y internal coordinates. 
+ * Normally, it is used by 0 in both values.
+ * 
+ * ** width and height values :
+ * The width and height values are the real frame width and height of the face image. 
+ * it means width and height values of the first frame in the sprite sheet.
+ * Normally, They are set as 144 px in both.
+ * 
+ * ** cols value : 
+ * Next, You should set the cols value in the sprite sheets.
+ * For example, if there are four cols and two rows in the sprite sheet.
+ * The number of frames will be 8 and then cols value is to 4.
+ * 
+ * ** maxFrames value : 
+ * if you set the maxFrames in custom data, you can want to limit a max frame 
+ * less than originally frames.
+ * 
+ * ** delay value :
+ * The delay time is the elapsed time since the previous frame.
+ * 1.0 will be around 0.016s
+ * For Example, 5.0 will be around 0.08s (0.016s * 5.0 = 0.08s)
+ * 
+ * ** looping value :
+ * if you set the looping value is to true, The animated face will never stop.
+ * it is played forever until the message would be terminated.
+ * 
+ * ================================================================
+ * Plugin Commands
+ * ================================================================
+ * 
+ * if you use this plugin command, you can show up an animated face in the message window.
+ * 
+ * ShowAnimationFace
+ * 
+ * it is returned back as a normal face if you use this plugin command.
+ * 
+ * HideAnimationFace
+ * 
+ * 애니메이션 얼굴 이미지의 설정 정보는 Set Animation Face 매개변수에서 설정합니다.
+ * AnimationFace에는 ID 값이 존재합니다. 
+ * ID는 빠르게 데이터를 취득 하기 위해 존재하는 데이터 식별 값으로 ID라고 부릅니다.
+ * 이 값은 문자열이나 숫자 값 등을 지정할 수 있습니다.
+ * 
+ * SetAnimationFace Id
+ * 
+ * 예를 들어, 매개변수에서 char라는 Id 값에 해당하는 JSON 데이터를 만들어놓았다고 가정합니다.
+ * 이 경우에는 문장의 표시를 호출하기 전에 다음 플러그인 명령을 사용해야 합니다.
+ * 
+ * ShowAnimationFace
+ * SetAnimationFace char
+ * 
+ * 문장의 표시에선 해당 얼굴 이미지를 찾아 선택해야 합니다.
+ * 옵션 정보에 맞게 얼굴 이미지를 자르고 프레임을 만들기 때문입니다.
+ * 
+ * 혹 다시 일반 얼굴 이미지를 사용하고 싶다면, 다음 플러그인 명령을 호출해야 합니다.
+ * 
+ * HideAnimationFace
+ * 
+ * 위 명령을 호출하면 다시 기본 상태로 되돌아갈 것입니다.
+ * 
+ * 회전, 위치 변경, 스케일 변경 기능에 대해서 설명하겠습니다.
+ * 이 기능은 전역에 적용되므로 매우 조심스럽게 사용해야 합니다.
+ * 모든 인자는 소문자로 적어야 합니다.
+ * 
+ * 예를 들어,스프라이트를 60도 회전을 시키고 싶다면 다음과 같이 해야 합니다.
+ * 
+ * ChangeParamAnimationFace angle 60.0
+ * 
+ * 메시지 윈도우 내에서 애니메이션 얼굴 이미지의 위치는 기본적으로
+ * 메시지 윈도우의 상대 좌표로 결정됩니다. 
+ * 따라서 상단 좌표는 0, 0 입니다.
+ * 
+ * ChangeParamAnimationFace x 0.0
+ * ChangeParamAnimationFace y 0.0
+ * 
+ * 애니메이션 얼굴 이미지의 크기는 scale 인자를 전달하여 조절이 가능한데요.
+ * 1.0은 기본 스케일이며, 2.0은 두 배 확대입니다.
+ * 
+ * ChangeParamAnimationFace scale 1.0
+ * 
+ * 다시 한 번 말하지만 위 인자들은 글로벌에 적용되기 때문에,
+ * 이후에 표시되는 모든 얼굴 애니메이션에 적용되므로 조심히 사용하시기 바랍니다.
+ * 
+ * ================================================================
+ * Change Log
+ * ================================================================
+ * 2019.07.16 (v1.0.0) - First Release.
+ * 2019.07.17 (v1.0.1) :
+ * - Fixed the issue that shows up the incorrect frame in the sprite sheet.
+ */
+/*~struct~AnimationFace:
+ *
+ * @param id
+ * @desc 해시 테이블을 이 ID 값으로 탐색하여 상태를 빠르게 취득합니다.
+ * @default 0
+ * 
+ * @param x
+ * @type number
+ * @desc 화면 상의 좌표가 아니라, 스프라이트 시트 내에서의 시작점을 의미합니다.
+ * @default 0
+ * 
+ * @param y
+ * @type number
+ * @desc 화면 상의 좌표가 아니라, 스프라이트 시트 내에서의 시작점을 의미합니다.
+ * @default 0
+ * 
+ * @param width
+ * @type number
+ * @desc 화면에 표시 할 프레임의 가로 길이입니다.
+ * @default 144
+ * 
+ * @param height
+ * @type number
+ * @desc 화면에 표시 할 프레임의 세로 길이입니다.
+ * @default 144
+ * 
+ * @param cols
+ * @type number
+ * @desc 열의 수. 애니메이션을 하려면 스프라이트 시트에서 한 프레임만을 잘라야 합니다.
+ * @default 4
+ * 
+ * @param maxFrames
+ * @type number
+ * @desc 최대 프레임입니다. 기본적으로 애니메이션 상태는 2 프레임 이상입니다.
+ * @default 3
+ * 
+ * @param delay
+ * @type number
+ * @desc 한 프레임이 끝나면 일정 시간 딜레이를 걸어줘야 자연스럽습니다.
+ * @default 5.00
+ * @decimals 2
+ * 
+ * @param looping
+ * @type boolean
+ * @desc 반복 재생을 위한 옵션입니다. 재생이 끝나면 반복적으로 재생되어야 하는 경우가 있습니다.
+ * @default false
+ * @on true
+ * @off false
+ *
+ */
+/*:ko
+ * @plugindesc This plugin allows you to show up the animation face in the messsage window <RS_FaceAnimation>
+ * @author biud436
+ * 
+ * @param Set Animation Face
+ * @type struct<AnimationFace>[]
  * @desc Animation Face에 대한 옵션 데이터를 설정할 수 있습니다.
  * @default ["{\"id\":\"blink\",\"x\":\"0\",\"y\":\"0\",\"width\":\"144\",\"height\":\"144\",\"cols\":\"4\",\"maxFrames\":\"3\",\"delay\":\"5.00\",\"looping\":\"true\"}"]
  * 
@@ -72,7 +241,7 @@
  * 2019.07.17 (v1.0.1) :
  * - Fixed the issue that shows up the incorrect frame in the sprite sheet.
  */
-/*~struct~AnimationFace:
+/*~struct~AnimationFace:ko
  *
  * @param id
  * @desc 해시 테이블을 이 ID 값으로 탐색하여 상태를 빠르게 취득합니다.
@@ -289,6 +458,7 @@ RS.FaceAnimation = RS.FaceAnimation || {};
             const delay = this._spriteData._frameDelay;
 
             const now = performance.now();
+
             const elapsed = (now - this._last) / 16.666666666666;
 
             if(endFrame - startFrame > 0) {
