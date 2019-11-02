bc5f9cb30b84b7e7e13d1c23680a48e83e28ed54
biud436 <biud436@gmail.com>
2019-11-02 18:07:29 +0900

    Fix F9 button is not working

--- nwjs/nwjs-v0.12.3-win-ia32/playGame.bat
+++ nwjs/nwjs-v0.12.3-win-ia32/playGame.bat
@@ -5,7 +5,7 @@ set ARGS1=%~dp1
 ECHO Current Path : %~dp0
 
 ECHO RUN INDEX FILE : %ARGS1%index.html
-%~dp0nw.exe --nwapp --url=%ARGS1%index.html
+%~dp0nw.exe --nwapp --url=%ARGS1%index.html?test
 
 @ENDLOCAL
 GOTO :EOF
