b0d7d37a572457584e257329f9cd786df6c62387
biud436 <biud436@gmail.com>
2019-11-06 17:10:02 +0900

    Add config for App Builder

---
+++ Android/AdmobSettings.js
@@ -0,0 +1,6 @@
+/*:
+ * You must change below ADMOB_APP_ID in case of using Cordova APP Builder.
+ *
+ * @cordova_plugin cordova-plugin-admob-free --save --variable ADMOB_APP_ID="ca-app-pub-7949707452244334~9380799608"
+ *
+ */
