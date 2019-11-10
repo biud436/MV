a9a4cd8c04a8fae8e020dc9e37e4d2aabb6d4b7d
biud436 <biud436@gmail.com>
2019-11-10 17:43:51 +0900

    Fix comment

--- Android/AdmobSettings.js
+++ Android/AdmobSettings.js
@@ -1,6 +1,8 @@
 /*:
- * You must change below ADMOB_APP_ID in case of using Cordova APP Builder.
- *
+ * 
+ * @plugindesc This provides a comment to inject the key in MV Cordova App Builder.
+ * @author biud436
  * @cordova_plugin cordova-plugin-admob-free --save --variable ADMOB_APP_ID="ca-app-pub-7949707452244334~9380799608"
- *
+ * @help
+ * You open this file and must change below <ADMOB_APP_ID="">
  */
