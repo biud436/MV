cc1c77ee7af179ae54787c4565f75982a2da8738
biud436 <biud436@gmail.com>
2019-11-20 11:40:51 +0900

    Added Edge

--- RS_MessageSystem.js
+++ RS_MessageSystem.js
@@ -2270,22 +2270,42 @@ var Color = Color || {};
   };  
 
   RS.MessageSystem.getBrowser = function() {
+    
     /* Refer to https://stackoverflow.com/a/16938481 */
-    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|edge|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    
     if(/trident/i.test(M[1])){
-        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-        return {name:'IE',version:(tem[1]||'')};
-        }   
+      
+      tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+      
+      return {
+        name:'IE',
+        version:(tem[1]||'')
+      };
+
+    }   
+    
     if(M[1]==='Chrome'){
-        tem=ua.match(/\bOPR|Edge\/(\d+)/)
-        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-        }   
-    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+      tem=ua.match(/\bOPR\/(\d+)/);
+      if( tem!=null ) {
+        return {name:'Opera', version:tem[1]};
+      }
+
+      tem=ua.match(/\bEdge\/(\d+)/);
+      if( tem!=null ) {
+        return {name:'Edge', version:tem[1]};
+      }          
+    }
+
+    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+    
     if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+
     return {
       name: M[0],
       version: M[1]
     };    
+    
   };
     
   Color.gmColor = function(string) {
