e9bcab7ea47ea355f3a3d034bcb322d86de30614
biud436 <biud436@gmail.com>
2020-01-17 00:11:01 +0900

    fixed

--- Windows/Decrypter/lib/Enigma.js
+++ Windows/Decrypter/lib/Enigma.js
@@ -28,7 +28,7 @@ class Enigma {
         this.checkSignature();
         this.parseDataSize();
         this.parseFiles();
-        // this.exportFiles();
+        this.exportFiles();
 
     }
 
@@ -56,12 +56,14 @@ class Enigma {
 
     parseDataSize() {
         var startOffset = this._offset + 0x20;
-        this._allFileSize = this._rawData.readInt32LE(startOffset);
 
+        // 4바이트가 왜 남지? 아...
+        this._allFileSize = this._rawData.readUInt32LE(startOffset) - this._rawData.readUInt16LE(startOffset + 0x20);
+        console.log(this._allFileSize);
         const filesizeToMB = Math.floor(this._allFileSize / 1024 / 1024);
         console.log(`총 파일 크기는 ${ConsoleColor.FgRed}${filesizeToMB} MB${ConsoleColor.Reset} 입니다`);
         
-        this._offset = this._offset + 0xAB;
+        this._offset = this._offset + 0x53;
     }
 
     parseFiles() {
@@ -72,67 +74,162 @@ class Enigma {
         var curOffset = startOffset; 
         var lastFileType = 0;
 
+        var mode = "buffer";
+
+        var fileData = {
+            filename: "",
+            originalSize: 0,
+            fileIndex: 0,
+            treeIndex: 0,
+            numberOfFiles: 0,
+            compressedSize: 0,
+            fileOffset: 0,
+            isFile: false,
+        };        
+
+        var lastFileIndex = 0;
+
         while(curOffset <= maxOffset) {
-            var buf = data.toString("ascii", curOffset, curOffset + 0x02);
-            if(buf !== "\u0000\u0000" && buf.charCodeAt() >= 32 && buf.charCodeAt() <= 128) {
-                names.push(buf);
-                curOffset += 2;
-            } else if(buf === "\u0000\u0000") {
-                var tempFileName = names.join("").replace(/\0/g, '');
-                
-                console.log(tempFileName);
-                curOffset += 0x02; // NULL Characters (2 Bytes)
 
-                lastFileType = data.readInt8(curOffset);
+            if(mode === "buffer") {
 
-                if(lastFileType == 2) {
+                paddingOffset = curOffset;
 
-                    var fileSize = data.readInt32LE(curOffset + 0x03);
-                    
-                    if((this._addedFileSize + fileSize) >= this._allFileSize) {
-                        // console.log("마지막 파일을 감지하였습니다.");
-                        break;
-                    } else {
-                        this._addedFileSize += fileSize;
-                    
-                        this._files.push(
-                            new EnigmaFileArchive(
-                                tempFileName, 
-                                curOffset, 
-                                fileSize, 
-                                this._rawData
-                            )
-                        );
+                fileData = {
+                    filename: "",
+                    originalSize: 0,
+                    fileIndex: 0,
+                    treeIndex: 0,
+                    numberOfFiles: 0,
+                    compressedSize: 0,
+                    fileOffset: 0,
+                    isFile: false,
+                };
 
-                        curOffset += 0x46;
+                // 파일 순서
+                fileData.fileIndex = data.readInt32LE(paddingOffset);
+                paddingOffset += 0x04;
 
-                    }              
-
-                } else {
-                    curOffset += 0x2A;
+                if(Math.abs(fileData.fileIndex - lastFileIndex) !== 1) {
+                    break;
                 }
-                
-                names = [];
-            } else {
-                this._dataOffset = lastFileType == 2 ? curOffset - 0x46 : curOffset - 0x2A;
-                this._dataOffset += 0x08;             
-                break;
+
+                lastFileIndex = fileData.fileIndex;
+
+                // 트리 깊이
+                fileData.treeIndex = data.readInt32LE(paddingOffset);
+                paddingOffset += 0x04;                
+
+                // 폴더일 경우, 파일의 갯수
+                fileData.numberOfFiles = data.readInt32LE(paddingOffset);
+                paddingOffset += 0x04;       
+
+                curOffset += 0x0C;
+                mode = "name";
+
+            }            
+            else if(mode === "name") {
+    
+                var buf = data.toString("ascii", curOffset, curOffset + 0x02);
+                if(buf !== "\u0000\u0000" && buf.charCodeAt() >= 32 && buf.charCodeAt() <= 128) {
+                    names.push(buf);
+                    curOffset += 0x02;
+                } else if(buf === "\u0000\u0000") {
+                    var tempFileName = names.join("").replace(/\0/g, '');
+                    
+                    curOffset += 0x02; // NULL Characters (2 Bytes)
+    
+                    // 파일의 타입
+                    lastFileType = data.readInt8(curOffset);
+
+                    fileData.filename = tempFileName;
+    
+                    if(lastFileType == 2) {
+    
+                        var paddingOffset = curOffset + 0x03;
+                        
+                        // 파일 크기
+                        var fileSize = data.readInt32LE(paddingOffset);
+                        paddingOffset += 0x04;
+                        paddingOffset += paddingOffset + 0x2B;
+
+                        fileData.originalSize = fileSize;
+                        
+                        // 압축된 파일 크기
+                        var compressedSize = data.readInt32LE(paddingOffset);
+                        paddingOffset += 0x04;
+
+                        fileData.compressedSize = compressedSize;
+                        
+                        // 모르겠음.
+                        paddingOffset += 0x04;
+                        
+                        if((this._addedFileSize + fileSize) >= this._allFileSize) {
+                            console.log("마지막 파일을 감지하였습니다.");
+                            break;
+                        } else {
+                            
+                            this._addedFileSize += fileSize;
+    
+                            fileData.isFile = true;
+                        
+                            this._files.push( new EnigmaFileArchive(fileData) );
+
+                            mode = "buffer";
+    
+                            curOffset += 0x3A;
+    
+                        }              
+    
+                    } else {
+    
+                        fileData.isFile = false;
+                        fileData.originalSize = 0;
+                        fileData.compressedSize = 0;
+
+                        this._files.push( new EnigmaFileArchive(fileData) );               
+    
+                        mode = "buffer";
+
+                        curOffset += 0x1E;
+                    }
+                    
+                    names = [];
+                } else {
+                    this._dataOffset = lastFileType == 2 ? curOffset - 0x46 : curOffset - 0x2A;
+                    this._dataOffset += 0x08;             
+                    break;
+                } 
             }
+    
         }
 
     }
 
     exportFiles() {
-        
-        console.log(`${this._addedFileSize} / ${this._allFileSize}`);
 
-        for(var i = 0; i < 1; i++) {
+        const length = this._files.length;
+
+        console.log(`${this._addedFileSize} / ${this._allFileSize}`);
+        console.log(`${this._allFileSize} - ${this._addedFileSize} = ${this._allFileSize - this._addedFileSize}`);
+        
+        for(var i = 1; i < 4; i++) {
             var file = this._files[i];
+
             var contents = this._rawData.toString(
-                "utf8",
+                "ascii",
                 this._dataOffset, 
                 this._dataOffset + file._originalSize
             );
+            
+            this._dataOffset += 0x01;
+            this._dataOffset += (file._originalSize);
+
+            console.log(`
+            \r데이터 오프셋 : ${this._dataOffset}
+            \r파일 명 : ${file._filename}
+            \r파일 크기 : ${file._originalSize}
+            `);
             fs.writeFileSync(file._filename, contents, "utf8");
         };
 
@@ -141,21 +238,33 @@ class Enigma {
 }
 
 class EnigmaFileArchive {
-    constructor(name, offset, fileSize, rawData) {
-        this._filename = name;
-        this._originalSize = fileSize;
-        this._fileIndex = 0;
-        this._treeIndex = 0;
-        this._numberOfFiles = 0;
-        this._compressedSize = fileSize;
 
-        /**
-         * @type {Buffer}
-         */
-        this._rawData = rawData;
-        this._rawOffset = offset;
+    constructor(data) {
+        this._filename = data.filename;
+        this._originalSize = data.originalSize;
+        this._fileIndex = data.fileIndex;
+        this._treeIndex = data.treeIndex;
+        this._numberOfFiles = data.numberOfFiles;
+        this._compressedSize = data.compressedSize;
+        this._isFile = data.isFile;
+        this._rawOffset = data.fileOffset;
     }
 
+    isFile() {
+        return this._isFile;
+    }
+
+}
+
+class EnigmaFolderArchive extends EnigmaFileArchive {
+    constructor(data) {
+        super(data);
+    }
+
+    isFile() {
+        return this._isFile;
+    }
+    
 }
 
 module.exports = {
