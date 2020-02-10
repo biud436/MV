2d3b8c0d634925ab40832c5861ec2036aede7192
biud436 <biud436@gmail.com>
2020-02-10 14:44:18 +0900

    added python get_mv_tools.py rw appName, hint, name, path

--- Windows/ToolManager/get_mv_tools.py
+++ Windows/ToolManager/get_mv_tools.py
@@ -2,7 +2,6 @@
 # -*-coding: utf-8 -*-
 # Author : biud436
 
-
 import sys
 
 major_version = sys.version_info.major
@@ -13,15 +12,84 @@ elif major_version == 3:
 
 import os
 import platform
+import json
+import re
+
+"""
+
+Author : biud436
+Usage:
+	Open the command line and call here as follows.
+	
+		python get_mv_tools.py r
+		python get_mv_tools.py w jsonRaw
+		python get_mv_tools.py rw appName, hint, name, path
+
+"""
 
 class App:
 
+	def __init__(self):
+		self.commands = sys.argv
+		self.sub_key = r"Software\KADOKAWA\RPGMV"		
+
 	def run(self):
-		self.process_registry()
+		mode = self.commands[1]
+		if mode in ['r', 'w']:
+			self.process_registry()
+		elif mode in ['rw']:
+			self.data = self.read_json()
+			
+			self.is_valid = True if self.data is not None else False
+
+			if self.is_valid and self.commands.count >= 5:
+				args = self.commands[2:6]
+				new_data = self.make_item(*args)
+				if self.add(new_data):
+					print(self.data)
+		else:
+			print("Cannot find the mode in command line option")
+
+
+	def read_json(self):
+		key = OpenKey(HKEY_CURRENT_USER, self.sub_key)
+		value, regtype = QueryValueEx(key, "mvTools")
+		CloseKey(key)
+		return json.loads(value.encode("utf8")) if major_version == 2 else json.loads(value)
+
+	def write_json(self, value):
+		key = OpenKey(HKEY_CURRENT_USER, self.sub_key, 0, KEY_ALL_ACCESS)
+		value = json.dumps(value.encode("utf8")) if major_version == 2 else json.dumps(value) 
+		
+		if isinstance(value, basestring):
+			SetValueEx(key, "mvTools", 0, REG_SZ, value)
+			CloseKey(key)	
+
+	def make_item(self, appName, hint, name, path):
+		posix_path = re.sub(r"\\", "/", path, 0, re.I)
+		data = { 
+			'appName': appName,
+			'hint': hint,
+			'name': name, 
+			'path': posix_path,
+		}
+		return data
+
+	def add(self, data):
+		if self.data is None:
+			return False
+
+		# Array type chceck
+		if isinstance(self.data, list) is not True:
+			return False
+	
+		self.data.append(data)
+
+		return True
 	
 	def process_registry(self):
 
-		varSubkey = r"Software\KADOKAWA\RPGMV"
+		varSubkey = self.sub_key
 
 		# Read
 		if sys.argv[1] == "r":
@@ -37,6 +105,7 @@ class App:
 			if isinstance(value, basestring):
 				SetValueEx(key, "mvTools", 0, REG_SZ, value)
 				CloseKey(key)
+
 
 app = App()
 app.run()
