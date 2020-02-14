6b07a1707236713945a58b043c941539e8f52d72
biud436 <biud436@gmail.com>
2020-02-14 15:15:02 +0900

    .

--- RS_EX_ToolKit.js
+++ RS_EX_ToolKit.js
@@ -30,99 +30,102 @@ RS.EX_ToolKit = RS.EX_ToolKit || {};
     
     parameters = (parameters.length > 0) && parameters[0].parameters;
     
-    
-    /**
-     * 자동으로 타겟의 위치까지 화면을 스크롤합니다.
-     */
-    Game_Interpreter.prototype.moveToTargetEvent = function() {
-        let joinedText = $gameVariables.value(RS.HangulInput.Params.variableId);
-        let ret = $gameMap.events().filter(e => e.event().name === joinedText);
-        if(ret && ret[0]) {
-            const target = ret[0];
+    if(Imported.RS_HangulInput) {
             
-            var sx = $gameMap.deltaX($gamePlayer.x, target.x);
-            var sy = $gameMap.deltaY($gamePlayer.y, target.y);
-            
-            var dir = 0;
-            var dist = 0;
-            var speed = 6;     
-            var minDist = 6;
-            
-            var dist2 = $gameMap.distance($gamePlayer.x, $gamePlayer.y, target.x, target.y);
-            
-            var addScroll = (...args) => {
+        /**
+         * 문자를 입력 받고 맵에서 특정 이름을 가진 이벤트를 찾아냅니다.
+         * 찾아낸 이벤트의 위치까지 화면을 스크롤하고 말풍선을 띄웁니다.
+         */
+        Game_Interpreter.prototype.moveToTargetEvent = function() {
+            let joinedText = $gameVariables.value(RS.HangulInput.Params.variableId);
+            let ret = $gameMap.events().filter(e => e.event().name === joinedText);
+            if(ret && ret[0]) {
+                const target = ret[0];
                 
-                this._list.push({
-                    "code": 204,
-                    "indent": 0,
-                    "parameters": [
-                        args[0],
-                        args[1],
-                        args[2]
-                    ]
-                });
+                var sx = $gameMap.deltaX($gamePlayer.x, target.x);
+                var sy = $gameMap.deltaY($gamePlayer.y, target.y);
                 
-            };
-            
-            var addList = (...args) => {
-                args.forEach(e => {
-                    this._list.push(e);
-                })
-            };
-            
-            if(dist2 >= minDist) {
+                var dir = 0;
+                var dist = 0;
+                var speed = 6;     
+                var minDist = 6;
                 
-                this._list.splice(-1, 1);
+                var dist2 = $gameMap.distance($gamePlayer.x, $gamePlayer.y, target.x, target.y);
                 
-                if(Math.abs(sx) >= minDist) {
-                    dir = sx < 0 ? 6 : sx > 0 ? 4 : 0;
-                    dist = Math.abs(sx);      
-                    addScroll(dir, dist, speed);
-                }
-                
-                if(Math.abs(sy) >= minDist) {
-                    dir = sy < 0 ? 2 : sy > 0 ? 8 : 0;
-                    dist = Math.abs(sy);                   
-                    addScroll(dir, dist, speed);
-                }
-                
-                addList(
-                    { 
-                        "code": 230, // 대기
-                        "indent": 0,
-                        "parameters": [60]
-                    }, { 
-                        "code": 101, // 문장의 표시
+                var addScroll = (...args) => {
+                    
+                    this._list.push({
+                        "code": 204,
                         "indent": 0,
                         "parameters": [
-                            "",
-                            0,
-                            0,
-                            2
+                            args[0],
+                            args[1],
+                            args[2]
                         ]
-                    }, {
-                        "code": 101,
-                        "indent": 0,
-                        "parameters": [
-                            "",
-                            0,
-                            0,
-                            2
-                        ]
-                    }, {
-                        "code": 401,
-                        "indent": 0,
-                        "parameters": [
-                            `\\말풍선[${target.eventId()}] 아니 내 이름을 어떻게 알았지?`
-                        ]
-                    }, { // 끝
-                        "code": 0,
-                        "indent": 0,
-                        "parameters": []
+                    });
+                    
+                };
+                
+                var addList = (...args) => {
+                    args.forEach(e => {
+                        this._list.push(e);
+                    })
+                };
+                
+                if(dist2 >= minDist) {
+                    
+                    this._list.splice(-1, 1);
+                    
+                    if(Math.abs(sx) >= minDist) {
+                        dir = sx < 0 ? 6 : sx > 0 ? 4 : 0;
+                        dist = Math.abs(sx);      
+                        addScroll(dir, dist, speed);
                     }
-                    );
+                    
+                    if(Math.abs(sy) >= minDist) {
+                        dir = sy < 0 ? 2 : sy > 0 ? 8 : 0;
+                        dist = Math.abs(sy);                   
+                        addScroll(dir, dist, speed);
+                    }
+                    
+                    addList(
+                        { 
+                            "code": 230, // 대기
+                            "indent": 0,
+                            "parameters": [60]
+                        }, { 
+                            "code": 101, // 문장의 표시
+                            "indent": 0,
+                            "parameters": [
+                                "",
+                                0,
+                                0,
+                                2
+                            ]
+                        }, {
+                            "code": 101,
+                            "indent": 0,
+                            "parameters": [
+                                "",
+                                0,
+                                0,
+                                2
+                            ]
+                        }, {
+                            "code": 401,
+                            "indent": 0,
+                            "parameters": [
+                                `\\말풍선[${target.eventId()}] 아니 내 이름을 어떻게 알았지?`
+                            ]
+                        }, { // 끝
+                            "code": 0,
+                            "indent": 0,
+                            "parameters": []
+                        }
+                        );
+                    }
                 }
-            }
         };    
-        
-    })(RS.EX_ToolKit);
+    }
+
+})(RS.EX_ToolKit);
