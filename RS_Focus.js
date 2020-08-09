2dcbe9adb504b9e316b522e4ce2f491587f442ba
biud436 <biud436@gmail.com>
2020-08-09 14:58:05 +0900

    포커스 유지용 플러그인

---
+++ RS_Focus.js
@@ -0,0 +1,46 @@
+//================================================================
+// RS_Focus.js
+// ---------------------------------------------------------------
+// The MIT License
+// Copyright (c) 2020 biud436
+// ---------------------------------------------------------------
+// Free for commercial and non commercial use.
+//================================================================
+/*:
+ * @target MZ
+ * @plugindesc <RS_Focus>
+ * @author biud436
+ * @url https://biud436.tistory.com/
+ *          
+ * @help
+ *
+ */
+
+var Imported = Imported || {};
+Imported.RS_Focus = true;
+
+var RS = RS || {};
+RS.Focus = RS.Focus || {};
+
+(($) => {
+    
+    "use strict";
+
+    const pluginParams = $plugins.filter(i => {
+        return i.description.contains('<RS_Window_KorNameEdit>');
+    });
+
+    const pluginName = (pluginParams.length > 0) && pluginParams[0].name;
+    const parameters = (pluginParams.length > 0) && pluginParams[0].parameters;    
+
+    SceneManager.isGameActive = function() {
+        try {
+            if(Utils.isNwjs()) return true;
+            return window.top.document.hasFocus();
+        } catch (e) {
+            // SecurityError
+            return true;
+        }
+    };
+      
+})(RS.Focus);
