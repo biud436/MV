371fc750194807f6e5c81408f08defdd449447a3
biud436 <biud436@gmail.com>
2021-09-08 16:59:57 +0900

    fixed style to "style"

--- TextboxBuilder.test.js
+++ TextboxBuilder.test.js
@@ -105,7 +105,7 @@ class TextBoxBuilder {
 
             // set injected style.
             if (root.injectedStyle) {
-                elem.setAttribute(style, root.injectedStyle);
+                elem.setAttribute("style", root.injectedStyle);
             }
 
             // set children.
