35d2f8a62e8f2afa60ec3d9ec5f7844d439cee57
biud436 <biud436@gmail.com>
2021-11-01 22:41:21 +0900

    removed certain methods in Window_NameBox

--- InDevelopment/RS_MessageSystem.js
+++ InDevelopment/RS_MessageSystem.js
@@ -3981,7 +3981,6 @@ RS.MessageSystem = RS.MessageSystem || {};
     };
 
     // TODO: 큰 얼굴 이미지 구현 필요
-    //
 
     /**
      * @param {String} faceName
@@ -4033,37 +4032,6 @@ RS.MessageSystem = RS.MessageSystem || {};
     // Game_Interpreter
     //============================================================================
 
-    /**
-     * @class Window_NameBox
-     */
-    Window_NameBox.prototype.initialize = function () {
-        Window_Base.prototype.initialize.call(this, new Rectangle());
-        this.openness = 0;
-        this._name = "";
-    };
-
-    Window_NameBox.prototype.setMessageWindow = function (messageWindow) {
-        this._messageWindow = messageWindow;
-    };
-
-    Window_NameBox.prototype.setName = function (name) {
-        if (this._name !== name) {
-            this._name = name;
-            this.refresh();
-        }
-    };
-
-    Window_NameBox.prototype.clear = function () {
-        this.setName("");
-    };
-
-    Window_NameBox.prototype.start = function () {
-        this.updatePlacement();
-        this.updateBackground();
-        this.createContents();
-        this.refresh();
-    };
-
     Window_NameBox.prototype.updatePlacement = function () {
         this.width = this.windowWidth();
         this.height = this.windowHeight();
@@ -4078,31 +4046,6 @@ RS.MessageSystem = RS.MessageSystem || {};
         } else {
             this.y = messageWindow.y + messageWindow.height;
         }
-    };
-
-    Window_NameBox.prototype.updateBackground = function () {
-        this.setBackgroundType($gameMessage.background());
-    };
-
-    Window_NameBox.prototype.windowWidth = function () {
-        if (this._name) {
-            const textWidth = this.textSizeEx(this._name).width;
-            const padding = this.padding + this.itemPadding();
-            const width = Math.ceil(textWidth) + padding * 2;
-            return Math.min(width, Graphics.boxWidth);
-        } else {
-            return 0;
-        }
-    };
-
-    Window_NameBox.prototype.windowHeight = function () {
-        return this.fittingHeight(1);
-    };
-
-    Window_NameBox.prototype.refresh = function () {
-        const rect = this.baseTextRect();
-        this.contents.clear();
-        this.drawTextEx(this._name, rect.x, rect.y, rect.width);
     };
 
     //============================================================================
