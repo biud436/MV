2cdc8a42ea0664fb2d9037fcf281fc2a63b89486
biud436 <biud436@gmail.com>
2021-11-08 00:02:25 +0900

    remove var

--- RS_AnimataionVisibility.js
+++ RS_AnimataionVisibility.js
@@ -7,21 +7,22 @@
 // Free for commercial and non commercial use.
 //================================================================
 /*:
+ * @target MV
  * @plugindesc This plugin allows you to remove the animation on the screen when the character is deactivated. <RS_AnimataionVisibility>
  * @author biud436
  * @help
  * ================================================================
  * Introduction
  * ================================================================
- * In case of the RPG Maker MV, 
+ * In case of the RPG Maker MV,
  * The Animation will be played until the last cell even if the character doesn't show up anymore on the screen.
  * This plugin allows you to remove the animation on the screen when the character is deactivated.
- * 
+ *
  * ◆ Animation Stop Condition
  *   - erased event.
  *   - if the event execution conditions are not met.
  *   - empty graphic.
- * 
+ *
  * ================================================================
  * Version Log
  * ================================================================
@@ -42,12 +43,12 @@
  * ================================================================
  * RPG Maker MV의 경우, 캐릭터가 화면에서 보이지 않아도 애니메이션이 끝까지 재생됩니다.
  * 이 플러그인은 캐릭터가 화면에서 보이지 않으면 애니메이션을 바로 정지시킵니다.
- * 
+ *
  *  ◆ 정지 조건​
  *    - 이벤트 일시 삭제
  *    - 이벤트 실행 조건이 충족하지 않을 때 (변수, 스위치, 셀프 스위치)
  *    - 캐릭터 그래픽이 비어있을 때
- * 
+ *
  * ================================================================
  * Version Log
  * ================================================================
@@ -59,57 +60,51 @@
  * 2020.09.17 (v1.0.3) :
  * - 타일 이벤트에서 애니메이션이 동작하지 않는 문제를 수정하였습니다.
  */
- 
-var Imported = Imported || {};
-Imported.RS_AnimataionVisibility = true;
- 
-(function() {
-  
-  var parameters = $plugins.filter(function(i) {
-    return i.description.contains("<RS_AnimataionVisibility>");
-  })[0].parameters;
-  
-  var alias_Sprite_Animation_update = Sprite_Animation.prototype.update;
-  Sprite_Animation.prototype.update = function() {
-    alias_Sprite_Animation_update.call(this);
-    this.updateVisibility();
-  };
 
-  Sprite_Animation.prototype.isTargetReady = function() {  
-    if(!this._target) return false;
-    if(!(this._target instanceof Sprite_Character)) return false;
-    const target = this._target._character;
+(() => {
+    const parameters = $plugins.filter(function (i) {
+        return i.description.contains("<RS_AnimataionVisibility>");
+    })[0].parameters;
 
-    var isActivated = false;
+    const alias_Sprite_Animation_update = Sprite_Animation.prototype.update;
+    Sprite_Animation.prototype.update = function () {
+        alias_Sprite_Animation_update.call(this);
+        this.updateVisibility();
+    };
 
-    if(target instanceof Game_Event) {
-      isActivated = target.findProperPageIndex() > -1;
-    } else if(target instanceof Game_CharacterBase) {
-      isActivated = true;
-    } else {
-      return false;
-    }
+    Sprite_Animation.prototype.isTargetReady = function () {
+        if (!this._target) return false;
+        if (!(this._target instanceof Sprite_Character)) return false;
+        const target = this._target._character;
 
-    var isTransparent = target.isTransparent(); // 투명한가?
-    var isErased = target._erased;
-    
-    if(!isErased) {
-      isErased = target.isTile() ? isErased : !target._characterName;      
-    }
-    
+        let isActivated = false;
 
-    return isActivated && !isTransparent && !isErased;
+        if (target instanceof Game_Event) {
+            isActivated = target.findProperPageIndex() > -1;
+        } else if (target instanceof Game_CharacterBase) {
+            isActivated = true;
+        } else {
+            return false;
+        }
 
-  };
-  
-  Sprite_Animation.prototype.updateVisibility = function() {
-    this.visible = this.isTargetReady();
-  };
+        const isTransparent = target.isTransparent(); // 투명한가?
+        const isErased = target._erased;
 
-  var alias_Sprite_Animation_processTimingData = Sprite_Animation.prototype.processTimingData;
-  Sprite_Animation.prototype.processTimingData = function(timing) {
-    if(!this.isTargetReady()) return;
-    alias_Sprite_Animation_processTimingData.call(this, timing);
-  };
-  
+        if (!isErased) {
+            isErased = target.isTile() ? isErased : !target._characterName;
+        }
+
+        return isActivated && !isTransparent && !isErased;
+    };
+
+    Sprite_Animation.prototype.updateVisibility = function () {
+        this.visible = this.isTargetReady();
+    };
+
+    const alias_Sprite_Animation_processTimingData =
+        Sprite_Animation.prototype.processTimingData;
+    Sprite_Animation.prototype.processTimingData = function (timing) {
+        if (!this.isTargetReady()) return;
+        alias_Sprite_Animation_processTimingData.call(this, timing);
+    };
 })();
