9d38faee1d347e736e808cb65a6226021fca72ca
biud436 <biud436@gmail.com>
2021-12-24 11:20:00 +0900

    adde DependencyInjector class

--- packages/decorators/alias.decorator.ts
+++ packages/decorators/alias.decorator.ts
@@ -1,43 +1,43 @@
 import "reflect-metadata";
 
 export function afterAlias(...args: any[]): MethodDecorator {
-    return (
-        target: object,
-        key: string | symbol,
-        descriptor: TypedPropertyDescriptor<any>
-    ) => {
-        const injectFunction = descriptor.value;
-        const METADATA_KEY = `${target}__${key.toString()}`;
+  return (
+    target: object,
+    key: string | symbol,
+    descriptor: TypedPropertyDescriptor<any>
+  ) => {
+    const injectFunction = descriptor.value;
+    const METADATA_KEY = `${target}__${key.toString()}`;
 
-        if (!Reflect.getMetadata(METADATA_KEY, target)) {
-            Reflect.defineMetadata(METADATA_KEY, args[0][key], target);
-        }
+    if (!Reflect.getMetadata(METADATA_KEY, target)) {
+      Reflect.defineMetadata(METADATA_KEY, args[0][key], target);
+    }
 
-        descriptor.value = function (...args: any[]) {
-            Reflect.getMetadata(METADATA_KEY, target).apply(this, args);
-            injectFunction.apply(this, args);
-        };
-        return descriptor;
+    descriptor.value = function (...args: any[]) {
+      Reflect.getMetadata(METADATA_KEY, target).apply(this, args);
+      injectFunction.apply(this, args);
     };
+    return descriptor;
+  };
 }
 
 export function beforeAlias(...args: any[]): MethodDecorator {
-    return (
-        target: object,
-        key: string | symbol,
-        descriptor: TypedPropertyDescriptor<any>
-    ) => {
-        const injectFunction = descriptor.value;
-        const METADATA_KEY = `${target}__${key.toString()}`;
+  return (
+    target: object,
+    key: string | symbol,
+    descriptor: TypedPropertyDescriptor<any>
+  ) => {
+    const injectFunction = descriptor.value;
+    const METADATA_KEY = `${target}__${key.toString()}`;
 
-        if (!Reflect.getMetadata(METADATA_KEY, target)) {
-            Reflect.defineMetadata(METADATA_KEY, args[0][key], target);
-        }
+    if (!Reflect.getMetadata(METADATA_KEY, target)) {
+      Reflect.defineMetadata(METADATA_KEY, args[0][key], target);
+    }
 
-        descriptor.value = function (...args: any[]) {
-            injectFunction.apply(this, args);
-            Reflect.getMetadata(METADATA_KEY, target).apply(this, args);
-        };
-        return descriptor;
+    descriptor.value = function (...args: any[]) {
+      injectFunction.apply(this, args);
+      Reflect.getMetadata(METADATA_KEY, target).apply(this, args);
     };
+    return descriptor;
+  };
 }
