9960e51dc92579acca1d4d26263a232a403c0690
biud436 <biud436@gmail.com>
2022-01-07 11:51:04 +0900

    migrated with ES6

--- RS_GhostEffect.js
+++ RS_GhostEffect.js
@@ -9,16 +9,16 @@
 /*:
  * @plugindesc This plugin allows you to implement the character effect like as ghost. <RS_GhostEffect>
  * @author biud436
- * 
+ *
  * @param Uniform
- * 
+ *
  * @param lifeTime
  * @parent Uniform
  * @type number
  * @desc All of the effects has a lifeTime, After that time, the effect pattern will be changed.
  * @default 100
  * @min 1
- * 
+ *
  * @param threshold
  * @parent Uniform
  * @type number
@@ -27,47 +27,47 @@
  * @decimals 2
  * @min 0.10
  * @max 1.00
- * 
+ *
  * @param xoffset
  * @parent Uniform
  * @type number
  * @desc When the value approaches 0.0, it would look like the paper burning.
  * @default 0.07
  * @decimals 2
- * 
+ *
  * @help
- * 
+ *
  * Note that this plugin only works in WebGL mode.
- * 
+ *
  * =======================================================
  * Scripts Call
  * =======================================================
  * In the event command called 'Set Move Route',
- * 
+ *
  * To activate ghost mode, you call the following script code:
  * this.ghostModeOn();
- * 
+ *
  * To disable ghost mode, you call the following script code:
  * this.ghostModeOff();
- * 
+ *
  * =======================================================
  * Plugin Commands
  * =======================================================
- * All of the effects has a lifeTime. 
+ * All of the effects has a lifeTime.
  * After that time, the effect pattern will be changed.
- * The time unit is milliseconds, 
+ * The time unit is milliseconds,
  * so setting it to 100 will result in 0.1 second.
- * 
+ *
  *   GhostEffect lifetime 100
- * 
+ *
  * If the threshold is close to 1, the effect may be reduced.
- * 
+ *
  *      GhostEffect threshold 0.7
- * 
+ *
  * When the value approaches 0.0, it would look like the paper burning.
- * 
+ *
  * GhostEffect xoffset 0.07
- * 
+ *
  * =======================================================
  * Version Log
  * =======================================================
@@ -80,43 +80,43 @@ Imported.RS_GhostEffect = true;
 var RS = RS || {};
 RS.GhostEffect = RS.GhostEffect || {};
 
-(function($) {
-    
+(() => {
     "use strict";
-    
-    var parameters = $plugins.filter(function (i) {
-        return i.description.contains('<RS_GhostEffect>');
+
+    let parameters = $plugins.filter((i) => {
+        return i.description.contains("<RS_GhostEffect>");
     });
-    
-    parameters = (parameters.length > 0) && parameters[0].parameters;
-    
+
+    parameters = parameters.length > 0 && parameters[0].parameters;
+
     RS.GhostEffect.Params = RS.GhostEffect.Params || {};
 
     RS.GhostEffect.Params.lifeTime = parseInt(parameters["lifeTime"] || 100);
-    RS.GhostEffect.Params.threshold = parseFloat(parameters["threshold"] || 0.7);
+    RS.GhostEffect.Params.threshold = parseFloat(
+        parameters["threshold"] || 0.7
+    );
     RS.GhostEffect.Params.xoffset = parseFloat(parameters["xoffset"] || 0.07);
-    
+
     //============================================================================
     // PIXI.GhostEffect
-    //============================================================================  
-    
-    PIXI.GhostEffect = function() {
-        
-        var vertexSrc = [
-            'attribute vec2 aVertexPosition;',
-            'attribute vec2 aTextureCoord;',
-            
-            'uniform mat3 projectionMatrix;',
-            
-            'varying vec2 vTextureCoord;',
-            
-            'void main(void){',
-            '    gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);',
-            '    vTextureCoord = aTextureCoord;',
-            '}'
-        ].join('\n');
-        
-        var fragmentSrc = `
+    //============================================================================
+
+    PIXI.GhostEffect = function () {
+        const vertexSrc = [
+            "attribute vec2 aVertexPosition;",
+            "attribute vec2 aTextureCoord;",
+
+            "uniform mat3 projectionMatrix;",
+
+            "varying vec2 vTextureCoord;",
+
+            "void main(void){",
+            "    gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);",
+            "    vTextureCoord = aTextureCoord;",
+            "}",
+        ].join("\n");
+
+        const fragmentSrc = `
         
         precision mediump float;
         
@@ -145,38 +145,45 @@ RS.GhostEffect = RS.GhostEffect || {};
             
             gl_FragColor = baseColor * recColor;
         }
-        
         `;
-        
-        PIXI.Filter.call( this, vertexSrc, fragmentSrc);
-        
+
+        PIXI.Filter.call(this, vertexSrc, fragmentSrc);
+
         this.uniforms.dimensions = new Float32Array(2);
         this.uniforms.u_scale = [0.5, 0.5];
         this.uniforms.u_xoffset = 0.07;
 
         this._effectVal = 0;
+
         this._time = performance.now();
-        
+
         this.enabled = true;
         this.resolution = 1;
-        
     };
-    
-    PIXI.GhostEffect.prototype = Object.create( PIXI.Filter.prototype );
+
+    PIXI.GhostEffect.prototype = Object.create(PIXI.Filter.prototype);
     PIXI.GhostEffect.prototype.constructor = PIXI.GhostEffect;
-    
-    PIXI.GhostEffect.prototype.apply = function(filterManager, input, output, clear) {
+
+    PIXI.GhostEffect.prototype.apply = function (
+        filterManager,
+        input,
+        output,
+        clear
+    ) {
         this.uniforms.dimensions[0] = input.sourceFrame.width;
         this.uniforms.dimensions[1] = input.sourceFrame.height;
-        
+
         filterManager.applyFilter(this, input, output, clear);
     };
-    
-    PIXI.GhostEffect.prototype.updateEffect = function() {
-        if(performance.now() - this._time < RS.GhostEffect.Params.lifeTime) return;
+
+    PIXI.GhostEffect.prototype.updateEffect = function () {
+        const isInvalidUpdateEffect =
+            performance.now() - this._time < RS.GhostEffect.Params.lifeTime;
+        if (isInvalidUpdateEffect) return;
+
         this._effectVal = Math.random();
-        
-        if(this._effectVal > RS.GhostEffect.Params.threshold) {
+
+        if (this._effectVal > RS.GhostEffect.Params.threshold) {
             this._effectVal = RS.GhostEffect.Params.threshold;
         }
         this.uniforms.u_scale[0] = this._effectVal;
@@ -185,107 +192,109 @@ RS.GhostEffect = RS.GhostEffect || {};
 
         this._time = performance.now();
     };
-    
+
     //============================================================================
     // Game_CharacterBase
-    //============================================================================  
-    
-    var alias_Game_CharacterBase_initMembers = Game_CharacterBase.prototype.initMembers;
-    Game_CharacterBase.prototype.initMembers = function() {
+    //============================================================================
+
+    const alias_Game_CharacterBase_initMembers =
+        Game_CharacterBase.prototype.initMembers;
+    Game_CharacterBase.prototype.initMembers = function () {
         alias_Game_CharacterBase_initMembers.call(this);
         this._isGhost = false;
     };
 
-    Game_CharacterBase.prototype.isGhost = function() {
+    Game_CharacterBase.prototype.isGhost = function () {
         return this._isGhost;
     };
 
-    Game_CharacterBase.prototype.ghostModeOn = function() {
+    Game_CharacterBase.prototype.ghostModeOn = function () {
         this._isGhost = true;
     };
 
-    Game_CharacterBase.prototype.ghostModeOff = function() {
+    Game_CharacterBase.prototype.ghostModeOff = function () {
         this._isGhost = false;
-    };    
+    };
 
-    Game_Player.prototype.ghostModeOn = function() {
+    Game_Player.prototype.ghostModeOn = function () {
         this._isGhost = true;
-        this._followers.forEach(function(follower) {
+        this._followers.forEach((follower) => {
             follower.ghostModeOn();
-        }, this);
+        });
     };
 
-    Game_Player.prototype.ghostModeOff = function() {
+    Game_Player.prototype.ghostModeOff = function () {
         this._isGhost = false;
-        this._followers.forEach(function(follower) {
+        this._followers.forEach((follower) => {
             follower.ghostModeOff();
-        }, this);
-    };        
-    
+        });
+    };
+
     //============================================================================
     // Sprite_Character
-    //============================================================================    
-    
-    var alias_Sprite_Character_initialize = Sprite_Character.prototype.initialize;
-    Sprite_Character.prototype.initialize = function(character) {
+    //============================================================================
+
+    const alias_Sprite_Character_initialize =
+        Sprite_Character.prototype.initialize;
+    Sprite_Character.prototype.initialize = function (character) {
         alias_Sprite_Character_initialize.call(this, character);
         this.createGhostEffect();
     };
-    
-    var alias_Sprite_Character_update = Sprite_Character.prototype.update;
-    Sprite_Character.prototype.update = function() {
+
+    const alias_Sprite_Character_update = Sprite_Character.prototype.update;
+    Sprite_Character.prototype.update = function () {
         alias_Sprite_Character_update.call(this);
         this.updateGhostEffect();
     };
-    
-    Sprite_Character.prototype.createGhostEffect = function() {
-        
-        var isValid = this._GhostEffect;
-        
-        if(!isValid) {
+
+    Sprite_Character.prototype.createGhostEffect = function () {
+        const isValid = this._GhostEffect;
+
+        if (!isValid) {
             this._GhostEffect = new PIXI.GhostEffect();
-            if(!this.filters) {
+            if (!this.filters) {
                 this.filters = [];
             }
             this.filters = [this._GhostEffect].concat(this.filters);
         } else {
-            if(!this.filters) {
+            if (!this.filters) {
                 this.filters = [];
             }
-            this.filters = this.filters.filter(function(filter) {
+            this.filters = this.filters.filter(function (filter) {
                 return filter !== isValid;
-            }, this);        
+            }, this);
         }
-    };
-    
-    Sprite_Character.prototype.updateGhostEffect = function() {
-        if(!$gameSystem) return;
-        if(!this._GhostEffect) return;
-        if(!this._character) return;
-        var isValid = this._character.isGhost();
-        this._GhostEffect.enabled = isValid;
-        if(isValid) {
-            this._GhostEffect.updateEffect();
-        }
-    
     };
 
-    var alias_Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    Sprite_Character.prototype.updateGhostEffect = function () {
+        if (!$gameSystem) return;
+        if (!this._GhostEffect) return;
+        if (!this._character) return;
+        const isValid = this._character.isGhost();
+        this._GhostEffect.enabled = isValid;
+        if (isValid) {
+            this._GhostEffect.updateEffect();
+        }
+    };
+
+    const alias_Game_Interpreter_pluginCommand =
+        Game_Interpreter.prototype.pluginCommand;
+    Game_Interpreter.prototype.pluginCommand = function (command, args) {
         alias_Game_Interpreter_pluginCommand.call(this, command, args);
-        if(command === "GhostEffect") {
-            switch(args[0]) {
-            case 'lifetime':
-                RS.GhostEffect.Params.lifeTime = Number(args[1] || 100);
-                break;
-            case 'threshold':
-                RS.GhostEffect.Params.threshold = parseFloat(args[1] || 0.7);
-                break;
-            case 'xoffset':
-                RS.GhostEffect.Params.xoffset = parseFloat(args[1] || 0.07);
-                break;
+        if (command === "GhostEffect") {
+            switch (args[0]) {
+                case "lifetime":
+                    RS.GhostEffect.Params.lifeTime = Number(args[1] || 100);
+                    break;
+                case "threshold":
+                    RS.GhostEffect.Params.threshold = parseFloat(
+                        args[1] || 0.7
+                    );
+                    break;
+                case "xoffset":
+                    RS.GhostEffect.Params.xoffset = parseFloat(args[1] || 0.07);
+                    break;
             }
         }
-    };    
-    
+    };
 })(RS.GhostEffect);
