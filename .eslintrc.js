047758ae2f06fe723c48fdbf6b4d92da7fa4b8bf
biud436 <biud436@gmail.com>
2022-06-25 01:39:10 +0900

    chore: added eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: ['airbnb-base', 'plugin:prettier/recommended'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'no-undef': 'off',
+        'no-use-before-define': 'off',
+        'func-names': 'off',
+        camelcase: 'off',
+        'no-param-reassign': 'off',
+        'no-plusplus': 'off',
+        'no-underscore-dangle': 'off',
+        'no-console': 'off',
+        'spaced-comment': 'off',
+        'max-classes-per-file': 'off',
+    },
+};
