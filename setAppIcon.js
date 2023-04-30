12bc8c6291b5c424599242eb9ec536646ec96d68
biud436 <biud436@gmail.com>
2023-04-30 12:32:49 +0900

    refactor: apply eslint

--- setAppIcon.js
+++ setAppIcon.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable no-unused-vars */
 //================================================================
 // setAppIcon.js
 // ---------------------------------------------------------------
@@ -9,54 +11,50 @@
 /*:
  * @plugindesc <setAppIcon>
  * @author biud436
- *          
+ *
  * @help
  *
  */
 
-var Imported = Imported || {};
-Imported.setAppIcon = true;
-
 function setAppIcon(filename) {
+    'use strict';
 
-    "use strict";
-
-    if(!Utils.isNwjs()) {
+    if (!Utils.isNwjs()) {
         return;
     }
-    
+
     const fs = require('fs');
     const path = require('path');
-    const cp = require("child_process");
+    const cp = require('child_process');
     const root = path.dirname(process.mainModule.filename);
-    const relativePath = (name)=>{ return path.join(root, name)};
-    
-    if(!fs.existsSync(relativePath(filename))) {
+    const relativePath = name => {
+        return path.join(root, name);
+    };
+
+    if (!fs.existsSync(relativePath(filename))) {
         console.warn(`Cannot find the icon image at ${filename}`);
         return;
     }
 
     // Copies the manifest.
-    let data = {
-        "name": "",
-        "main": "index.html",
-        "js-flags": "--expose-gc",
-        "window": {
-            "title": "",
-            "toolbar": false,
-            "width": 816,
-            "height": 624,
-            "icon": filename
-        }
+    const data = {
+        name: '',
+        main: 'index.html',
+        'js-flags': '--expose-gc',
+        window: {
+            title: '',
+            toolbar: false,
+            width: 816,
+            height: 624,
+            icon: filename,
+        },
     };
-    
-    // Stringify the data that changed app icon
-    let contents = JSON.stringify(data, null, "\t");
 
-    fs.writeFileSync(relativePath("package.json"), contents, "utf8");
+    // Stringify the data that changed app icon
+    const contents = JSON.stringify(data, null, '\t');
+
+    fs.writeFileSync(relativePath('package.json'), contents, 'utf8');
 
     // Restart the game.
     chrome.runtime.reload();
-
-}  
-    
+}
