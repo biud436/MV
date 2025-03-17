ce327648f76524be90f705805542d649cfb607f6
biud436 <biud436@gmail.com>
2025-03-17 10:38:21 +0900

    fix: Remove Korean comments and documentation from multiple plugin files

--- RS_BattleStaticAnimation.js
+++ RS_BattleStaticAnimation.js
@@ -58,68 +58,6 @@
  * - Added a new feature that can play the animation without setting the battler.
  * - Fixed the issue that is not played the animation correctly on the center of the screen.
  */
-/*:ko
- * @target MV
- * @plugindesc This plugin allows you to play the loop animation during the battle <RS_BattleStaticAnimation>
- * @author biud436
- *
- * @help
- *
- * 플러그인을 구동 시 YEP_CoreEngine과 YEP_BattleEngineCore.js 파일이 필요하므로,
- * YEP_CoreEngine과 YEP_BattleEngineCore 플러그인 아래 어딘가에 플러그인을 배치하시기 바랍니다.
- *
- * 전투 도중에 특정 애니메이션을 반복 재생합니다.
- * 애니메이션의 원점은 애니메이션 설정에 따라 달라집니다.
- * 원점이라는 건 데이터베이스 애니메이션 탭에서 위치 값을 뜻합니다.
- * '상단'으로 설정되어있을 경우, 타겟의 머리 위에서 재생되고
- * '하단'으로 설정되어있을 경우, 타겟의 다리 쪽에서 재생되는 것이 보통입니다.
- * '중앙'으로 설정되어있을 경우, 타겟의 중앙에 재생됩니다.
- * '화면'이라면 전투 필드의 중앙 지점에 설정됩니다. 즉, 쉽게 말하면 화면 중앙에 표시됩니다.
- *
- * PlayStaticAnimation uniqId battlerId animationId
- *      - uniqId : 애니메이션 제거 시에 필요한 식별자를 문자열로 지정하세요.
- *      - battlerId : 배틀러 ID가 -일 경우, Enemy의 ID 값을 뜻하고 +면 아군의 ID 값을 뜻합니다.
- *      - animationId : 애니메이션의 ID 값을 지정하세요. 이 값은 1부터 시작해야 합니다.
- *
- * 배틀러의 ID는 데이터베이스에서의 ID 값이 아닌, 턴제 전투에서의 아군과 적그룹 내에서의 인덱스입니다.
- * 인덱스는 0이 아닌 1부터 시작하며, 파티원 1에 애니메이션을 표시하려면 1을 전달해야 합니다.
- * 반면 적군 1에 애니메이션을 표시하려면 -1을 전달해야 합니다.
- * 배틀러 ID 값에 해당하는 배틀러가 없는 경우에는 애니메이션이 재생되지 않습니다.
- *
- * 애니메이션 반복 재생은 배틀러가 도중에 죽었을 땐 반복이 중단됩니다.
- * 전투 애니메이션은 통상적으로 모든 배틀러의 앞 쪽에 위치합니다.
- *
- * 배틀러와 상관 없이 화면에 항상 재생하고 싶다면 다음 플러그인 명령을 사용해주시기 바랍니다.
- * 다음 플러그인 명령은 화면 중앙에 특정 애니메이션을 반복 재생합니다.
- * 또한 애니메이션의 원점을 중앙으로 강제 변경하고 화면 중앙에 애니메이션을 표시합니다.
- * 애니메이션의 원점은 데이터베이스에서 애니메이션 탭에서 '위치'에 해당하는 값입니다.
- *
- * PlayScreenAnimation uniqId ox oy animationId
- *      - uniqId : 애니메이션 제거 시에 필요한 식별자를 문자열로 지정하세요.
- *      - ox and oy : 기본적으로는 화면 중앙에 표시되지만 오프셋 좌표를 설정하면
- *                    화면 중앙을 기점으로 상대적으로 좌표를 조절할 수 있습니다.
- *                    이 기능이 필요하지 않은 경우, 매개변수 값을 0으로 설정하시기 바랍니다.
- *      - animationId : 애니메이션의 ID 값을 지정하세요. 이 값은 1부터 시작해야 합니다.
- *
- * StopStaticAnimation uniqId
- *      - uniqId : 애니메이션 제거 시에 필요한 식별자를 문자열로 지정하세요.
- *
- * 애니메이션을 제거하지 않아도 전투가 종료될 때 알아서 제거됩니다.
- *
- * 이 플러그인은 의도적으로 ES6 문법을 위주로 사용하였기 때문에
- * RPG Maker MV v1.6.1 이상에서만 사용할 수 있습니다.
- *
- * =====================================================================
- * Change Log
- * =====================================================================
- * 2019.09.11 (v1.0.0) - First Release.
- * 2019.10.10 (v1.0.1) :
- * - 프론트 뷰에서 애니메이션이 재생되지 않는 문제를 수정하였습니다.
- * - YEP_CoreEngine 의존성에 대한 설명이 추가되었습니다.
- * 2019.10.11 (v1.0.2) :
- * - 배틀러 설정 없이 애니메이션을 표시할 수 있는 기능을 추가하였습니다.
- * - 화면 중앙에 표시되지 않는 문제를 수정하였습니다.
- */
 (() => {
   const RS = window.RS || {};
   RS.BattleStaticAnimation = RS.BattleStaticAnimation || {};
